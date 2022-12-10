1799788bdaa9c57ac8b15dd48fe9f09f355ddb45
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-11 04:48:34 +0530

    Time: 444 ms (30.91%), Space: 57.2 MB (62.67%) - LeetHub

---
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int head=0,tail=0,ans=0,result=0;
+        for(head=0;head<nums.size();head++){
+            
+            while((ans&nums[head])>0){
+                ans^=nums[tail];
+                tail++;
+            }
+            
+            ans|=nums[head];
+            result=max(result,head-tail+1);
+            
+        }
+        return result;
+    }
+};
