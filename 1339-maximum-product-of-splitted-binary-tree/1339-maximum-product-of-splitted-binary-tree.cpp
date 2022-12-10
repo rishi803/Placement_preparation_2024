e3981b802c96cfdabec26831df07742af4de1080
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-10 19:15:12 +0530

    Time: 353 ms (13.17%), Space: 77.3 MB (74.21%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    long total=0,ans=0,every_sub_tree_sum;
+    int maxProduct(TreeNode* root) {
+        total=total_sum_remain_tree(root);
+        total=total_sum_remain_tree(root);
+        return ans% (int)(1e9+7);
+    }
+    
+    int total_sum_remain_tree(TreeNode* root){
+        if(!root) return 0;
+        every_sub_tree_sum=root->val+total_sum_remain_tree(root->left)+total_sum_remain_tree(root->right);
+         ans=max(ans,every_sub_tree_sum*(total-every_sub_tree_sum));
+        return every_sub_tree_sum;
+    }
+  
+};
