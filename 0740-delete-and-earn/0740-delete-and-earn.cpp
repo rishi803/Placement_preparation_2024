680bc3047878cefae099c862da47a31fb64eee5c
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-14 00:03:33 +0530

    Time: 22 ms (72.17%), Space: 16.5 MB (18.64%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int DP(vector<int>& arr, vector<int>& dp, int n, int ind){
-        if(ind >= n) return 0;
-        
-        if(dp[ind] != -1) return dp[ind];
-        
-        int i = ind;
-        int sum = 0;
-        while(i < n && arr[i] == arr[ind]){
-            sum += arr[ind];
-            i++;
-        }
-        
-        while(i < n && arr[i] == arr[ind] + 1)
-            i++;
-        
-        int pick = sum + DP(arr, dp, n, i);
-        int notPick = DP(arr, dp, n, ind + 1);
-        return dp[ind] = max(pick,notPick);
-        
-    }
+    
     int deleteAndEarn(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n + 1, -1);
-        sort(nums.begin(),nums.end());
-        return DP(nums, dp, n, 0);
+        int n = 10001;
+    
+	//take the total sum by each number
+    vector<int> sum(n, 0);
+    vector<int> dp(n, 0);
+    
+    for(auto num: nums){
+        sum[num] += num;
+    }
+    
+    dp[0] = 0;
+    dp[1] = sum[1];
+    //now apply the house robbing concept
+    for(int i=2; i<n; i++){
+        dp[i] = max(dp[i-2] + sum[i], dp[i-1]);
+    }
+    
+    return dp[n-1];
         
     }
 };
