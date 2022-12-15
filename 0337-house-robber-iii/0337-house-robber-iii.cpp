040d014ec2c01fa2416c1b799d327e1595b2b023
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-15 20:47:20 +0530

    Time: 61 ms (9.06%), Space: 28.4 MB (23.82%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -4,9 +4,9 @@ public:
     unordered_map<TreeNode*,vector<int>>mp;
     int help(TreeNode* root,bool valid){
         if(!root) return 0;
-        if(mp.count(root) and mp[root][valid]!=0) return mp[root][valid];
-        mp[root]={0,0};
-        int rob = valid?root->val+help(root->left,false)+help(root->right,false):0;
+        if(mp.count(root) and mp[root][valid]!=-1) return mp[root][valid];
+        mp[root]={-1,-1};
+        int rob = valid?root->val+help(root->left,false)+help(root->right,false):-1;
         int notrob=help(root->left,true)+help(root->right,true);
         return mp[root][valid]= max(rob,notrob);
     }
