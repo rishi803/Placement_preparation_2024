c64c402f74f82c099d4120641e04d19fae13a019
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-15 20:24:10 +0530

    Time: 45 ms (50.42%), Space: 13.3 MB (12.25%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int lcs(int i,int j,string &text1, string &text2, vector<vector<int>>& dp)
+    {
+        if(i<0 || j<0)    return 0;
+        if(dp[i][j] != -1)  return dp[i][j];
+        if(text1[i] == text2[j])    return dp[i][j] = 1+lcs(i-1,j-1,text1,text2,dp);  // if character matches
+        return dp[i][j] = max(lcs(i-1,j,text1,text2,dp), lcs(i,j-1,text1,text2,dp)); // if character not matches
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        
+        int n1 = text1.size();
+        int n2 = text2.size();
+        
+        vector<vector<int>> dp(n1,vector<int>(n2,-1));
+        return lcs(n1-1,n2-1,text1,text2,dp);
+      
+    }
+};
