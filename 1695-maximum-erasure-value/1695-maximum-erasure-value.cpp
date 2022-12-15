bb4996c9232a08b6688150bb20cf27a231571dd1
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-16 03:45:21 +0530

    Time: 297 ms (93.45%), Space: 99.5 MB (82.65%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -4,15 +4,15 @@ public:
         int head=0,tail=0,ans=0,sum=0;
         unordered_map<int,int>mp;
         for(head=0;head<nums.size();head++){
-            mp[nums[head]]++;
-            if(mp[nums[head]]>1) ans=max(ans,sum);
-              sum+=nums[head];
+          mp[nums[head]]++;
+       
             while(mp[nums[head]]>1){
-                sum-=nums[tail];
+                ans=max(ans,sum);
                 mp[nums[tail]]--;
+                sum-=nums[tail];
                 tail++;
-                
             }
+             if(mp[nums[head]]==1) sum+=nums[head];
            
         }
         return max(ans,sum);
