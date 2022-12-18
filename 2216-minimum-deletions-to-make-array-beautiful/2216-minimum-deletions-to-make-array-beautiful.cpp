8b12fc88ee25ae9a90d12f1199873952524aa4cf
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-18 23:24:54 +0530

    Time: 582 ms (9.26%), Space: 121.1 MB (70.41%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
-//     int minDeletion(vector<int>& nums) {
-//         int cnt=0;
-//         for(int i=0;i<nums.size(); ){
-//              while(i<nums.size()-1 and i%2==0 and nums[i]==nums[i+1]){
-//                  nums.erase(nums.begin()+i);
-//                  cnt++;
-//                  if(nums[i]!=nums[i+1]) i++;
-//              }
-          
-//                 i++;
-            
-//         }
-//         return nums.size()%2!=0 ? cnt+1 : cnt;
-//     
-// }
+
  // above approach gives TLE as we are actually shifting all elements again and again.Actually we do not do this
 
 int minDeletion(vector<int> &nums) {
