4a804b963af7fab6800214e0059414e646bded9d
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-19 21:30:06 +0530

    Time: 1371 ms (42.25%), Space: 126.6 MB (89.24%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+      vector<int> parent;     
+    
+    int findParent(int node) {
+        if(parent[node]==node) return node;
+        return parent[node]=findParent(parent[node]);
+    }    
+    
+    void makeSameGroup(int u , int v) {
+        int pu = findParent(u) ; 
+        int pv = findParent(v);
+        parent[pu] = pv; 
+    }
+    
+    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
+        parent.resize(n);
+        for(int i=0;i<n;i++)
+            parent[i] = i; 
+        
+        for(auto e : edges) {
+            makeSameGroup(e[0] , e[1]); 
+        }                         
+        return findParent(start) == findParent(end); 
+    }
+};
