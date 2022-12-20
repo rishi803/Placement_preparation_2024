6bf7ee729b26411f1d43c5fad7285d99823fdf69
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-20 23:28:04 +0530

    Time: 24 ms (28.67%), Space: 10.5 MB (35.11%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -10,14 +10,9 @@ public:
     }
     
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        vector<vector<int>>adj(rooms.size());
-        for(int i=0;i<rooms.size();i++){
-            for(int j=0;j<rooms[i].size();j++){
-                adj[i].push_back(rooms[i][j]);
-            }
-        }
+       
         visited.resize(rooms.size(),0);
-        help(adj,0);
+        help(rooms,0);
         if(count(visited.begin(),visited.end(),1)==rooms.size()) return true;
         else return false;
         
