7fbbea6f56a61d53aeffb1ce75a62a9441a69788
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-24 23:48:40 +0530

    Time: 6 ms (35.39%), Space: 7.6 MB (57.30%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/swap-nodes-in-pairs/discuss/1775033/SWAPPING-NODES-(Not-just-the-values)-oror-Visual-Explanation-oror-Well-Explained-oror-C%2B%2B
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head or !head->next) return head;
+        ListNode* dummy= new ListNode (0);
+        ListNode* prev=dummy;
+        ListNode* curr=head;
+        
+        while(curr and curr->next){
+            prev->next=curr->next;
+            curr->next=prev->next->next;
+            prev->next->next=curr;
+            
+            prev=curr;
+            curr=curr->next;
+        }
+        return dummy->next;
+    }
+};
