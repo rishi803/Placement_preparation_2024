358dc4b6877f489dd2c8275e8cc5b5cec1aa398b
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-26 23:00:32 +0530

    Time: 45 ms (93.23%), Space: 30.9 MB (91.35%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -4,7 +4,10 @@ public:
     bool help(vector<int>&arr, int st){
         if(st<0 or st>=arr.size() or arr[st]<0) return false;
         if(arr[st]==0) return true;
-        arr[st]*=-1;
+        arr[st]*=-1;                 // mark the index as visited and dont worry it will not effect below code
+                                     // beacause sign will interchange only which causes no effect below but very helpful 
+                                     // to mark it as visited that dont explore this path by next function because it only 
+                                     // ask for true or false not any integer answer.
         
        return  help(arr,st+arr[st]) or help(arr,st-arr[st]);
     }
