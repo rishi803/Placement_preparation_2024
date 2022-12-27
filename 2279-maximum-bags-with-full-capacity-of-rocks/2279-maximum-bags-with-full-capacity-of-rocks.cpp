e3ddb444a7cdd5f84be1e8d0131bd30a10c67065
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-27 22:08:54 +0530

    Time: 242 ms (73.85%), Space: 89.5 MB (50.33%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int i=0;i<capacity.size();i++){
+            pq.push((capacity[i]-rocks[i]));
+        }
+        int ans=0;
+        
+        while(!pq.empty()){
+            int f=pq.top();
+            pq.pop();
+            if(f==0) ans++;
+           else if(f>0 and additionalRocks-f>=0){
+               
+               additionalRocks-=f;
+               ans++;
+           }
+            
+             
+        }
+        return ans;
+    }
+};
