3b1c36db3c2820e87d08eeb637c1b9188edaec7f
rishi singh <85188638+rishi803@users.noreply.github.com>
2022-12-28 02:01:40 +0530

    Time: 59 ms (89.36%), Space: 45.5 MB (7.85%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int help(vector<int>weights,int wt,int days){
+       int cnt=1;
+        int sum=0;
+        for(int i=0;i<weights.size();i++){
+            sum+=weights[i];
+            if(sum>wt){
+                cnt++;
+                sum=weights[i];
+            }
+        }
+        if(cnt<=days) return true;
+        else return false;
+    }
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int high=accumulate(weights.begin(),weights.end(),0);
+        int low=*max_element(weights.begin(),weights.end());
+        int ans=0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (help(weights,mid, days)) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        
+        return ans;
+    }
+};
