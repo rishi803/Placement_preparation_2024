0b26405a5d3384dcbe89ba7bfc799fbc2d8f82fe
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-02 00:34:08 +0530

    Time: 273 ms (27.27%), Space: 18.9 MB (18.18%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int distinctPrimeFactors(vector<int>& nums) {
         
-        set<int>st;
+        unordered_set<int>st;
         int n=nums.size();
         
     for(int i=0;i<nums.size();i++){
