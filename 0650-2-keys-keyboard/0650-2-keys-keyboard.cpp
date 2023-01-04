01a371d75dea98f8059b2a98b30994203668528a
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-04 23:18:57 +0530

    Time: 30 ms (39.85%), Space: 5.9 MB (92.90%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    long long copy=1;
+    long long help(int n,long long value,long long copy){
+        if(value>n) return INT_MAX;
+        if(value==n) return 0;
+        
+     return  min(2+help(n,value+value,value),1+help(n,value+copy,copy));
+        
+        
+        
+    }
+    
+    int minSteps(int n) {
+        if(n==1) return 0;
+        if(n==2) return 2;
+        return  1+help(n,1,1);
+    }
+};
