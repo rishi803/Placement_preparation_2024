e3aa44ade1c5c0d461429e89953a4df7005a90b6
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-04 21:31:29 +0530

    Time: 6 ms (49.21%), Space: 5.8 MB (95.22%) - LeetHub

---
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int smallestValue(int n) {
+      
+        while(true){
+             int sum=0;
+             int prime=2,cnt=0;
+             int fix=n;
+            while(n>1){
+                if(n%prime==0) {
+                     sum+=prime;
+                    cnt++;
+                    n/=prime;
+                }
+                
+                else prime++;
+            }
+            if(sum==fix) return fix;
+            n=sum;
+            if(cnt<2) return n;
+        }
+        return -1;
+    }
+    
+};
