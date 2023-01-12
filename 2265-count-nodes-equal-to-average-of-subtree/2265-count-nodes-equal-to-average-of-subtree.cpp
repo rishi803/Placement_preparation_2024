7a96f41777a14413ab71acba12b6d3c1eb01961b
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-12 23:57:07 +0530

    Time: 7 ms (90.58%), Space: 12 MB (39.61%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+//     pair<int,int>help(TreeNode* root,int &cnt){
+//        if(!root) return {0,0};  
+       
+       
+//         auto left= help(root->left,cnt);
+//         auto right=help(root->right,cnt);
+//         int valans=left.first+right.first+root->val;
+//         int countans=left.second+right.second+1;
+//        if(((valans)/(countans))==root->val) cnt++;
+        
+//         return {valans,countans};
+        
+        
+//     }
+    
+     int ans = 0;
+//     pair<int,int> solve(TreeNode* root){
+//         if(root==NULL) return {0,0};
+        
+//         auto left = solve(root->left);
+//         int l_sum = left.first; // sum of nodes present in left sub tree
+//         int l_cnt = left.second; // no. of nodes present in left sub tree
+        
+//         auto right = solve(root->right);
+//         int r_sum = right.first; // sum of nodes present in right sub tree
+//         int r_cnt = right.second; // no. of nodes present in left sub tree
+        
+//         int sum = root->val+l_sum+r_sum;
+//         int cnt = l_cnt+r_cnt+1;
+        
+//         if(root->val == sum/cnt) ans++;
+//         return {sum,cnt};
+//     }
+    
+      pair<int,int> solve(TreeNode* root){
+        
+       if(!root) return {0,0};
+       auto left=solve(root->left);
+       auto right=solve(root->right);
+       int sum=root->val+left.first+right.first;
+       int cnt=left.second+right.second+1;
+       if(root->val==(sum)/cnt) ans++;
+       return {sum,cnt};
+    }
+    
+    int averageOfSubtree(TreeNode* root) {
+        solve(root);
+        return ans;
+    }
+    
+//     int averageOfSubtree(TreeNode* root) {
+//       int cnt=0;
+//          help(root,cnt);
+//         return cnt;
+      
+//     }
+};
