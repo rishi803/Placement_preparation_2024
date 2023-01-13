ad99aa09e2f4de056ef1d1498dba0981bf015e08
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-13 05:57:55 +0530

    Time: 67 ms (5.22%), Space: 28.4 MB (21.97%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -1,18 +1,35 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    unordered_map<TreeNode*,vector<int>>mp;
-    int help(TreeNode* root,bool valid){
-        if(!root) return 0;
-        if(mp.count(root) and mp[root][valid]!=-1) return mp[root][valid];
-        mp[root]={-1,-1};
-        int rob = valid?root->val+help(root->left,false)+help(root->right,false):-1;
-        int notrob=help(root->left,true)+help(root->right,true);
-        return mp[root][valid]= max(rob,notrob);
-    }
     
-    int rob(TreeNode* root) {
+    unordered_map<TreeNode*,vector<int>>dp;
+    
+    int help(TreeNode* root,bool canrob){
+        if(!root) return 0;
+        if(dp.count(root) && dp[root][canrob] != -1) return dp[root][canrob];
+        dp[root] = {-1,-1};
+        int takeroot=-1;
+            if(canrob)
+            takeroot=root -> val + help(root -> left, false) + help(root -> right, false);
         
-        return help(root,true);
+        int notroot= -1;
+          notroot=help(root->left,true)+help(root->right,true);
+        
+        return dp[root][canrob]=max(takeroot,notroot);
+    }
+    int rob(TreeNode* root) {
+        // memset(dp,-1,sizeof(dp));
+        return help(root,1);
+      
     }
 };
