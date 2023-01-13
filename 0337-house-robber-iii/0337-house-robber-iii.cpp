68a809233d2f64108c054014d30875adc302ba8a
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-13 22:36:55 +0530

    Time: 68 ms (5.22%), Space: 28.2 MB (24.21%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -10,24 +10,44 @@
  * };
  */
 
-// TEST CASE: [2,1,3,null,4],       [4,1,null,2,null,3]
+// TEST CASE: [2,1,3,null,4],       [4,1,null,2,null,]
 class Solution {
 public:
     
-    pair<int,int> help(TreeNode* root){
+//     pair<int,int> help(TreeNode* root){
         
-        if(!root) return {0,0};                        // pair(include,exclude)
-        pair<int,int>leftans=help(root->left);
-        pair<int,int>rightans=help(root->right);
+//         if(!root) return {0,0};                        // pair(include,exclude)
+//         pair<int,int>leftans=help(root->left);
+//         pair<int,int>rightans=help(root->right);
         
-        int include_root=root->val+leftans.second+rightans.second;
-        int exclude_root=max(leftans.first,leftans.second)+max(rightans.first,rightans.second);
+//         int include_root=root->val+leftans.second+rightans.second;
+//         int exclude_root=max(leftans.first,leftans.second)+max(rightans.first,rightans.second);
         
-        return {include_root,exclude_root};
-    }
+//         return {include_root,exclude_root};
+//     }
     
+//     int rob(TreeNode* root) {
+//         pair<int,int>ans=help(root);
+//         return max(ans.first,ans.second);
+//     }
+    
+    unordered_map<TreeNode*,vector<int>>dp;      
+    
+    int help(TreeNode* root,bool canrob){
+        if(!root) return 0;
+        if(dp.count(root) && dp[root][canrob] != -1) return dp[root][canrob];
+        dp[root] = {-1,-1};
+        int takeroot=0;
+            if(canrob)
+            takeroot=root -> val + help(root -> left, false) + help(root -> right, false);
+        
+        int notroot= 0;
+          notroot=help(root->left,true)+help(root->right,true);
+        
+        return dp[root][canrob]=max(takeroot,notroot);
+    }
     int rob(TreeNode* root) {
-        pair<int,int>ans=help(root);
-        return max(ans.first,ans.second);
+        return help(root,1);
+      
     }
 };
