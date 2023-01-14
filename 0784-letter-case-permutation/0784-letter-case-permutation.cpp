45629e2f1c2b51a7ba64360d749a6218ac9f8694
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-14 05:40:54 +0530

    Time: 4 ms (89.90%), Space: 10.5 MB (66.68%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -3,27 +3,20 @@ public:
     vector<string>ans;
     
      void help(string &s , int idx){
-        
-          ans.push_back(s);
-         
-        if(idx>=s.size()){
-           
+        if(idx==s.size()){
+            ans.push_back(s);
             return;
         }
-        for(int i=idx;i<s.size();i++){
-            
-            if(isalpha(s[i])){
-               
-             s[i]^=(1<<5);
-            
-            help(s,i+1);
-                
-             s[i]^=(1<<5);
-            
-            }
-           
-            
-        }
+         help(s,idx+1);                // not take
+         if(isalpha(s[idx])){           // take
+             s[idx]^=32;
+             help(s,idx+1);
+             s[idx]^=32;
+         }
+         
+         
+         
+         
     }
     vector<string> letterCasePermutation(string s) {
        
