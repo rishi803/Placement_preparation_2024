34e9cf5ab843af544b82d34a4afe6285e5eb7fc3
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-18 22:25:14 +0530

    Time: 121 ms (74.75%), Space: 67.8 MB (14.05%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,11 +5,11 @@ public:
         int mx=nums[0],cursum=0;
         
         for(int i:nums){
-            
+            if(cursum<0) cursum=0;
             cursum+=i;
             mx=max(mx,cursum);
             
-            if(cursum<0) cursum=0;
+            
         }
         return mx;
     }
