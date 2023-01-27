c5d0a00ef21747cfa1b60d0aba9b8bd759a975bd
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-27 05:47:20 +0530

    Time: 91 ms (5.03%), Space: 10.6 MB (42.78%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+       map<int,int>mp;
+        int passenger=0;
+        
+        for(auto &i:trips){
+            mp[i[1]]+=i[0];
+            mp[i[2]]+=-i[0];
+            
+            for(auto &it:mp){
+                passenger+=it.second;
+                if(passenger>capacity) return false;
+            }
+        }
+        return true;
+    }
+};
