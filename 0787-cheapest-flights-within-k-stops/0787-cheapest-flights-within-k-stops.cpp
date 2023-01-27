f23dfe46a10645fac9e0e683b2a6a289e9e0a6eb
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-28 05:06:45 +0530

    Time: 31 ms (64.94%), Space: 14 MB (29.42%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> graph(n);
+        vector<vector<pair<int, int>>> graph(n);       // vector<vector<dest,cost>>
         for(auto e : flights) {
             graph[e[0]].push_back({e[1], e[2]});
         }
         int ans=INT_MAX;
-        vector<int> dist(n, INT_MAX);
-        queue<pair<int, int>> q;
-        q.push({src, 0});
+        vector<int> cost(n, INT_MAX);
+        queue<pair<int, int>> q;                       
+        q.push({src, 0});                      // q<pair<dest,initial_cost>>
         int stops=0;
         while(stops<=k && !q.empty()) {
             int sz=q.size();
             while(sz--) {
-                int cnode=q.front().first;
-                int cdist=q.front().second;
+                int cur_node=q.front().first;
+                int cur_cost=q.front().second;
                 q.pop();
-                if(cdist>dist[cnode]) continue;
-                dist[cnode]=cdist;
-                for(auto e : graph[cnode]) {
-                    if(e.second+cdist>ans) continue;
-                    if(e.first==dst) ans=min(ans, e.second+cdist);
-                    q.push({e.first, e.second+cdist});
+                if(cur_cost>cost[cur_node]) continue;
+                cost[cur_node]=cur_cost;
+                for(auto &child : graph[cur_node]) {
+                    if(child.second+cur_cost>ans) continue;
+                    if(child.first==dst) ans=min(ans, child.second+cur_cost);
+                    q.push({child.first, child.second+cur_cost});
                 }
             }
             stops++;
