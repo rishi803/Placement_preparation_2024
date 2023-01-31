2b2e65e91358425007c5f6b60d40134040ebfaba
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-31 23:49:06 +0530

    Time: 1404 ms (5.19%), Space: 133.8 MB (14.61%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<int>>dp;
     int help(vector<pair<int,int>>&v,int prev,int idx){
         if((idx>=v.size())) return 0;
+        if((prev>-1 and  v[idx].second<v[prev].second)) return dp[prev+1][idx]=help(v,prev,idx+1);
         if(dp[prev+1][idx]!=-1) return dp[prev+1][idx];
         int take=0,nottake=0;
-        if(prev==-1||v[idx].second>=v[prev].second){
-        take+=v[idx].second+help(v,idx,idx+1);}
+        take+=v[idx].second+help(v,idx,idx+1);
         nottake+=help(v,prev,idx+1);
         
         return dp[prev+1][idx]=max(take,nottake);
