e7f2812b9e187ae2d6324cc0c780944bcc524390
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-01-31 23:50:50 +0530

    Time: 492 ms (23.05%), Space: 133.8 MB (13.64%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int help(vector<pair<int,int>>&v,int prev,int idx){
+   
+    int help(vector<pair<int,int>>&v,int prev,int idx,vector<vector<int>>&dp){
         if((idx>=v.size())) return 0;
-        if((prev>-1 and  v[idx].second<v[prev].second)) return dp[prev+1][idx]=help(v,prev,idx+1);
         if(dp[prev+1][idx]!=-1) return dp[prev+1][idx];
         int take=0,nottake=0;
-        take=v[idx].second+help(v,idx,idx+1);
-        nottake=help(v,prev,idx+1);
+        if(prev==-1||v[idx].second>=v[prev].second){
+        take=v[idx].second+help(v,idx,idx+1,dp);}
+        nottake=help(v,prev,idx+1,dp);
         
         return dp[prev+1][idx]=max(take,nottake);
     }
     
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+         vector<vector<int>>dp;
         vector<pair<int,int>>v;
         dp.resize(scores.size()+1,vector<int>(scores.size()+1,-1));
         for(int i=0;i<scores.size();i++){
             v.push_back({ages[i],scores[i]});
         }
         sort(v.begin(),v.end());
-        return help(v,-1,0);
+        return help(v,-1,0,dp);
     }
 };
