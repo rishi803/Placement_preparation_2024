3a08f50cdb49f2b6d38ecaf8ce5bf94534d99c0f
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-01 23:05:02 +0530

    Time: 333 ms (32.05%), Space: 22.1 MB (37.89%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,40 @@
+// Time: O(M*N log M*N)
+// Space: O(M*N)
+    
+// We need to store the efforts based on the row and column we are currently at. So by doing row*100 + col, we are essentially storing both the row number and column number in a single int. Else, we would have to take pair<int, pair<int, int>> for {effort, {row, col}}
+
+//  vector<vector<int>> dirs = {
+//                 {-1,0},
+//         {0,-1},         {0,1},
+//                 {1, 0}
+//     };
+
+
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>> &heights) {
+    int m = heights.size(), n = heights[0].size();
+    int dirs[5] = {-1, 0, 1, 0, -1};
+
+    std::vector<vector<int>> efforts(m, vector<int>(n, INT_MAX));
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    pq.emplace(0, 0); // First item is effort, second is row * 100 + col
+    while (!pq.empty()) {
+      int effort = pq.top().first;
+      int x = pq.top().second / 100, y = pq.top().second % 100;
+      pq.pop();
+
+      if (effort >= efforts[x][y]) continue;
+      efforts[x][y] = effort;
+
+      for (int i = 0; i < 4; i++) {
+        int nx = x + dirs[i], ny = y + dirs[i + 1];
+        if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+        int n_effort = max(effort, abs(heights[x][y] - heights[nx][ny]));
+        pq.emplace(n_effort, nx * 100 + ny);
+      }
+    }
+    return efforts[m-1][n-1];
+  }
+};
