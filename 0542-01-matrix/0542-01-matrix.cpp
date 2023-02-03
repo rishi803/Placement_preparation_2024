91aeaf870a12c2436e363702d6e996c5ded071b4
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-03 11:41:19 +0530

    Time: 88 ms (74.61%), Space: 30.2 MB (68.56%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -7,13 +7,13 @@ public:
         for (int r = 0; r < m; ++r)
         { for (int c = 0; c < n; ++c)
                 if (mat[r][c] == 0) q.emplace(r,c);
-                else mat[r][c] = -3; }// Marked as not processed yet!
+                else mat[r][c] = -1; }// Marked as not processed yet!
 
         while (!q.empty()) {
             auto [r, c] = q.front(); q.pop();
             for (int i = 0; i < 4; ++i) {
                 int nr = r + DIR[i], nc = c + DIR[i+1];
-                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -3) continue;
+                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
                 mat[nr][nc] = mat[r][c]+1;  //if we found -1 means it is 1 so to updatae it by adding +1 in its root grid after storing its ans so that it distance helps in finding distance for its surrounding -1 add+1
                 q.emplace(nr, nc);
             }
