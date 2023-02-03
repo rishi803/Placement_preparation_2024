6679bdd217642da6d640351830d03e853c045760
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-04 04:09:40 +0530

    Time: 5 ms (9.90%), Space: 7.1 MB (7.20%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    void help(vector<string>&ans,string digits,vector<string>mp,int idx,string &temp){
+        
+        if(idx==digits.size()){     // store the answer
+           ans.push_back(temp);
+            return;
+        }
+        
+        int num=digits[idx]-'0';   // extract the digit(converting in integer form)
+        
+        for(int i=0;i<mp[num].size();i++){  // always considering each mp[num] from 0 to its size
+           
+            temp+=mp[num][i];           // operation
+            help(ans,digits,mp,idx+1,temp); // call
+            temp.pop_back(); // backtrack
+        }
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        if(digits.size()==0) return {};
+        vector<string>mp={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string>ans;
+        string temp;
+        help(ans,digits,mp,0,temp);
+        return ans;
+    }
+};
