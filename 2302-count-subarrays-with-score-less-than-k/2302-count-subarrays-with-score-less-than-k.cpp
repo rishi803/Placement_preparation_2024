25826796f7b28f37ca6edd536738e7d106e23921
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-05 23:43:05 +0530

    Time: 150 ms (98.45%), Space: 95.1 MB (61.24%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
@@ -5,15 +5,14 @@ public:
         long long head=0,tail=0,ans=0,sum=0;
         
         for(head=0;head<nums.size();head++){
-            
             sum+=nums[head];
             
             while(sum*(head-tail+1)>=k){
-               
+                
                 sum-=nums[tail];
                 tail++;
             }
-             // cout<<sum*(head-tail+1)<<endl;
+            
             ans+=head-tail+1;
         }
         return ans;
