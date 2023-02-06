638ddcef678445ac70a5c438511630ea1d55b215
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-06 22:07:17 +0530

    Time: 34 ms (67.77%), Space: 13.1 MB (40.72%) - LeetHub

--- 2546-apply-bitwise-operations-to-make-strings-equal/2546-apply-bitwise-operations-to-make-strings-equal.cpp
+++ 2546-apply-bitwise-operations-to-make-strings-equal/2546-apply-bitwise-operations-to-make-strings-equal.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool makeStringsEqual(string s, string target) {
-        sort(s.begin(),s.end());
-        sort(target.begin(),target.end());
-        int num_target=0;
-        for(auto &i:target) num_target+=(i-'0');
         
         if(s==target) return true;
-        else if(num_target!=0){
-            if(find(s.begin(),s.end(),'1')!=s.end()) return true;
-        }
-        return false;
+        
+        if(count(s.begin(),s.end(),'1') and count(target.begin(),target.end(),'1')) return true;
+        else return false;
+        
     }
 };
