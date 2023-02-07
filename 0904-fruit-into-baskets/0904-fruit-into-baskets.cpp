ceca927848ff881b58c780065a5dced28183add2
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-07 19:43:35 +0530

    Time: 202 ms (43.67%), Space: 71.3 MB (48.83%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int>mp;
-        int head=0,tail=0,fruitcnt=0;
+        ; 
+        unordered_map<int,int>mp ;      // if necessity to use data structure then use (to store frequency)
+        int head=0,tail=0,fruitcnt=0;    // declaration of global variables
         
-        for(head=0;head<fruits.size();head++){
+        for(head=0;head<fruits.size();head++){   // scanning fruits array
             
             mp[fruits[head]]++;
+                             
             
-            while(mp.size()>2){
+            while(mp.size()>2){                // if condition fails go inside while loop to make it true
+                
                 mp[fruits[tail]]--;
-                if(mp[fruits[tail]]==0) mp.erase(fruits[tail]);  
-                tail++;
+                if(mp[fruits[tail]]==0) mp.erase(fruits[tail]);
+                tail++;                      // update tail
             }
             
-            fruitcnt=max(fruitcnt,head-tail+1);
+            fruitcnt=max(fruitcnt,head-tail+1);  // update the answer
         }
         
-        return fruitcnt;
+        return fruitcnt;             // return answer
     }
 };
