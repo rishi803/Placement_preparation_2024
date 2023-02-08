055c740eb5b353bb37ce16c48c0e68acf6abaf6a
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-09 01:32:26 +0530

    Time: 324 ms (14.59%), Space: 44.2 MB (5.58%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,30 +1,27 @@
-// using ---> lower bound by storing end-1 as it is excluded and start is included if lower bound of start is searched then end-1 will save us from wrong ans as it give first occurence and if not present then give next element iterator
-
-// using ---> upper bound by storing end as it is excluded and if upper bound of start is searched then it give iterator to next occurence of it whether element is present or not
-
-// using prefix sum trick
-
-// dry run for --  ["MyCalendar","book","book","book","book","book","book","book","book","book","book"]
-// [[],[47,50],[33,41],[39,45],[33,42],[25,32],[26,35],[19,25],[3,8],[8,13],[18,27]]
-
 class MyCalendar {
 public:
+    
     map<int,int>mp;
     MyCalendar() {
         
     }
     
     bool book(int start, int end) {
-        // cout<<"St = "<<start<<" End = "<<end<<" ";
-      auto it=mp.upper_bound(start);
-      if(it!=mp.end() and it->second<end) {
-          // cout<<"ubf "<<it->first<<" ";
-           return false;}
-       // else if(it!=mp.end()) cout<<"ub "<<it->first<<" ";
-        mp[end]=start;
-        // for(auto &it:mp){
-        //     cout<<it.first<<" "<<it.second<<endl;
-        // }
+        
+        mp[start]++;
+        mp[end]--;
+        
+        int sum=0;
+        
+        for(auto &it:mp){
+            sum+=it.second;
+            
+            if(sum>1){
+                mp[start]--;
+                mp[end]++;
+                return false;
+            }
+        }
         return true;
     }
 };
