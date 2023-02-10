55b29c036b3f06a029181716dfb049eacae24750
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-10 15:58:47 +0530

    Time: 58 ms (75.12%), Space: 26.1 MB (36.26%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,34 +1,52 @@
 class Solution {
 public:
-    int help(vector<int>&weights,int wt,int days){
-       int cnt=1;
-        int sum=0;
-        for(int i=0;i<weights.size();i++){
-            sum+=weights[i];
-            if(sum>wt){
-                cnt++;
-                sum=weights[i];
-            }
-        }
-        if(cnt<=days) return true;
-        else return false;
-    }
+    
+   bool isValid(vector<int>&weights,int days,int mid_weight){
+       
+       int days_required=1;
+       int sum=0;
+       
+       for(int i=0;i<weights.size();i++){
+           
+           sum+=weights[i];
+           
+           if(sum>mid_weight){
+               days_required++;
+               sum=weights[i];
+           }
+           
+       }
+       
+       return days_required<=days;
+       
+   }
     
     int shipWithinDays(vector<int>& weights, int days) {
         
-        int high=accumulate(weights.begin(),weights.end(),0);
-        int low=*max_element(weights.begin(),weights.end());
         int ans=0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (help(weights,mid, days)) {
-                ans = mid;
-                high = mid - 1;
+        
+        int low=*max_element(weights.begin(),weights.end());
+        int high=accumulate(weights.begin(),weights.end(),0);
+        
+        while(low<=high){
+            
+            int mid_weight=low+(high-low)/2;
+            
+            if(isValid(weights,days,mid_weight)){
+                
+                ans=mid_weight;
+                high=mid_weight-1;
             }
-            else
-                low = mid + 1;
+            
+            else{
+                
+                low=mid_weight+1;
+            }
+            
+            
         }
         
         return ans;
+        
     }
 };
