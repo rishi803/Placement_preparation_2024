04005322abbed02de649eda04453ecb603dcac60
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-11 22:54:53 +0530

    Time: 74 ms (63.62%), Space: 21.5 MB (49.32%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        
+        queue<pair<int,int>>q;          // q<pair<i,j>>
+        int n=grid.size();
+        
+        vector<vector<int>>vis=grid;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) q.push({i,j});   // store all the i,j where there is initally land
+            }
+        }
+        
+        if(q.size()==0 or q.size()==n*n) return -1;
+        
+        vector<int>direction={-1,0,1,0,-1};
+        int distance=-1;
+        
+        while(!q.empty()){
+            int sz=q.size();
+            
+            while(sz--){
+                auto [x,y]=q.front();
+                q.pop();
+                
+                for(int i=0;i<4;i++){
+                    int nx=x+direction[i];
+                    int ny=y+direction[i+1];
+                    
+            if(nx>=0 and nx<n and ny>=0 and ny<n and vis[nx][ny]==0){
+                vis[nx][ny]=1;
+                q.push({nx,ny});
+            }
+                }
+            }
+            distance++;
+        }
+        
+        return distance;
+    }
+};
