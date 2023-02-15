8c6101c5d387686fcb2a74ffb02e24d88b80dc20
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-15 16:17:26 +0530

    Time: 133 ms (78.59%), Space: 73.2 MB (28.47%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,3 +1,5 @@
+// agr elements decrease ho rhe to pichla max ans hi ans rhega kunki utna operation laga hi hoga lkin jaise element increase hona chalu ho to pichla ans + current difference jo aya
+
 class Solution {
 public:
     int minNumberOperations(vector<int>& target) {
@@ -13,19 +15,29 @@ public:
 //         }
 //         return dp[dp.size()-1];
         
-        stack<int>st;
-        int ans=0;
-        st.push(target[0]);
-        ans=target[0];
-        for(int i=1;i<target.size();i++){
-            
-            if( st.top()<target[i]){
-                ans+=target[i]-st.top();
-            }
-            
-            st.push(target[i]);
-        }
+        int ans=target[0];
         
+        for(int i=1;i<target.size();i++){
+             if(target[i]>target[i-1]) 
+            
+                 ans+=(target[i]-target[i-1]);
+            }
         return ans;
+        
+        
+//         stack<int>st;
+//         int ans=0;
+//         st.push(target[0]);
+//         ans=target[0];
+//         for(int i=1;i<target.size();i++){
+            
+//             if( st.top()<target[i]){
+//                 ans+=target[i]-st.top();
+//             }
+            
+//             st.push(target[i]);
+//         }
+        
+//         return ans;
     }
 };
