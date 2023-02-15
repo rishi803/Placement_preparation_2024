3f75ed5a97d9b2158806115f8af3e3fb1511e2bc
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-15 16:10:48 +0530

    Time: 177 ms (21.88%), Space: 77.4 MB (9.65%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -2,15 +2,30 @@ class Solution {
 public:
     int minNumberOperations(vector<int>& target) {
         
-        vector<int>dp(target.size());
-        dp[0]=target[0];
+//         vector<int>dp(target.size());
+//         dp[0]=target[0];
         
+//         for(int i=1;i<target.size();i++){
+//             if(target[i]<=target[i-1]) dp[i]=dp[i-1];
+//             else{
+//                  dp[i]=dp[i-1]+(target[i]-target[i-1]);
+//             }
+//         }
+//         return dp[dp.size()-1];
+        
+        stack<int>st;
+        int ans=0;
+        st.push(target[0]);
+        ans=target[0];
         for(int i=1;i<target.size();i++){
-            if(target[i]<=target[i-1]) dp[i]=dp[i-1];
-            else{
-                 dp[i]=dp[i-1]+(target[i]-target[i-1]);
+            
+            if( st.top()<target[i]){
+                ans+=target[i]-st.top();
             }
+            
+            st.push(target[i]);
         }
-        return dp[dp.size()-1];
+        
+        return ans;
     }
 };
