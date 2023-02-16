5c43fc51444a1fdb487bf43ade8556bf438a3444
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-16 22:54:35 +0530

    Time: 426 ms (40.84%), Space: 161 MB (26.26%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<long long>>dp;
+    
+    long long help(vector<int>&nums,int idx,bool flag){
+        
+        if(idx>=nums.size()) return 0;
+        if(dp[idx][flag]!=-1) return dp[idx][flag];
+        long long take=0,nottake=0;
+        
+        if(flag){
+             take=nums[idx]+help(nums,idx+1,false);
+            nottake=help(nums,idx+1,true);
+        }
+        else{
+            take=-nums[idx]+help(nums,idx+1,true);
+            nottake=help(nums,idx+1,false);
+        }
+        
+        return dp[idx][flag]=max(take,nottake);
+        
+    }
+    
+    long long maxAlternatingSum(vector<int>& nums) {
+        dp.resize(nums.size(),vector<long long>(2,-1));
+        return help(nums,0,true);
+    }
+};
