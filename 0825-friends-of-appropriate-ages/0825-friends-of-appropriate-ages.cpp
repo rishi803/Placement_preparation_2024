f1b71d8743cb43512c6d52154adf8cc4243f1470
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-16 14:18:10 +0530

    Time: 150 ms (11.94%), Space: 37.4 MB (44.78%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
@@ -5,19 +5,16 @@ public:
         sort(ages.begin(),ages.end());
          int ans=0;
         
-        
-          
-        
         for(int i=0;i<ages.size();i++){
             
-             int agey=(ages[i]/2)+7;
-            
-            
+             int agey=(ages[i]/2)+7;      // considering age[i] as x
             
             int idx1=upper_bound(ages.begin(),ages.end(),agey)-ages.begin();
-            int idx2=upper_bound(ages.begin(),ages.end(),ages[i])-ages.begin();
-            idx2--;
+            int idx2=upper_bound(ages.begin(),ages.end(),ages[i])-ages.begin();  // for duplicates
+            idx2--;     // always go to last index of that element
+            
             // cout<<idx1<<" "<<idx2<<endl;
+            
             if(idx2>idx1){
                 ans+=idx2-idx1;
             }
