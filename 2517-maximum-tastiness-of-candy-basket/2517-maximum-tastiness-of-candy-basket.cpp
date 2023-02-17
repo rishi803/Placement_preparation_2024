1ec52845d359c4962c9f9b3914026967a96f6d65
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-18 01:04:35 +0530

    Time: 222 ms (86.70%), Space: 48.5 MB (79.50%) - LeetHub

--- 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-      bool isans(vector<int>&temp, int n, int k, int mn) {
+      bool isans(vector<int>&temp, int n, int k, int mn) { 
       int cnt = 1;
       int last = temp[0];
-      for (int i = 1; i < n; i++) {
+      for (int i = 1; i < n; i++) {  
         if (temp[i] - last >= mn) {
           cnt++;
           last = temp[i];
@@ -14,20 +14,26 @@ public:
     }
     
     int maximumTastiness(vector<int>& price, int k) {
-        sort(price.begin(), price.end());
+        
+      sort(price.begin(), price.end());
+        
       int n=price.size();
-      int l = 1, h = price[n - 1] - price[0];
-
-      while (l <= h) {
+      int l = 1;     // decide lower range 
+      int  h = price[n - 1] - price[0];  // decide upper range
+  
+      int ans=0;
+        
+      while (l <= h) {            // do binary search 
         int mid = (l + h)/ 2;
 
         if (isans(price, n, k, mid)) {
+          ans=mid;
           l = mid + 1;
         } else {
           h= mid - 1;
         }
       }
       
-          return h;
+          return ans;
     }
 };
