337038be27e83ae9de34d4f20f9ace023e13012a
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-17 14:04:31 +0530

    Time: 4 ms (56.58%), Space: 8.3 MB (57.45%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int>mp;
-        unordered_map<int,int>mp1;
+        
+        unordered_map<int,int>priyanshu;
+        
         for(int i=0;i<arr.size();i++){
-            mp[arr[i]]++;
-        }
-        arr.clear();
-        for(auto &it:mp){
-            mp1[it.second]++;
+            priyanshu[arr[i]]++;
         }
         
-        return mp1.size()==mp.size();
+        unordered_set<int>st;
+        for(auto &[fir,sec]:priyanshu){
+            st.insert(sec);
+        }
+        
+        if(st.size()!=priyanshu.size()) return false;
+        return true;
     }
 };
