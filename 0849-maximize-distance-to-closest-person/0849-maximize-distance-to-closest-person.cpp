f65321c8885747e7c966cefec0ceb6b10983088d
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-20 22:54:10 +0530

    Time: 19 ms (33.04%), Space: 17 MB (94.25%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.cpp
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.cpp
@@ -1,28 +1,29 @@
+// hme 1 se 1 tk ka distance pr kam krna h
+
 class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
         
-       vector<int>temp;
-        
+        int left=-1;
+        int ans=0;
         for(int i=0;i<seats.size();i++){
-            if(seats[i]==1) temp.push_back(i);
             
-        }
-        
-        int mx=1,mn=INT_MAX;
-        for(int i=0;i<seats.size();i++){
-            mn=INT_MAX;
-            if(seats[i]==0){
-                for(int j=0;j<temp.size();j++){
-                    mn=min(mn,abs(i-temp[j]));
-                     // cout<<mn<<endl;
-                }
-                 // cout<<endl;
-                mx=max(mx,mn);
-               
+            if(i==seats.size()-1 and seats[i]==0){
+                ans=max(ans,i-left);
+            }
+            if(seats[i]==0) continue;
+            
+            if(left==-1){                          
+                ans=max(ans,i-0);
+                left=i;
+            }
+            
+            else{
+                ans=max(ans,(i-left)/2);
+                left=i;
             }
             
         }
-        return mx;
+        return ans;
     }
 };
