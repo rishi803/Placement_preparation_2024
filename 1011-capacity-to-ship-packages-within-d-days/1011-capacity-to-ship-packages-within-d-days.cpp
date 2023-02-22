c12c590b39da753da8105b3b5a3a66ded2a45ad9
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-22 18:10:23 +0530

    Time: 58 ms (73.24%), Space: 25.9 MB (96.08%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
-    
-   bool isValid(vector<int>&weights,int days,int mid_weight){
-       
-       int days_required=1;
-       int sum=0;
-       
-       for(int i=0;i<weights.size();i++){
-           
-           sum+=weights[i];
-           
-           if(sum>mid_weight){
-               days_required++;
-               sum=weights[i];
-           }
-           
-       }
-       
-       return days_required<=days;
-       
-   }
+    bool isValid(int mid_weight,vector<int>&weights,int days){
+        int cnt=1,sum=0;
+        
+        for(int i=0;i<weights.size();i++){
+            sum+=weights[i];
+            
+            if(sum>mid_weight){
+                cnt++;
+                sum=weights[i];
+            }
+        }
+        return cnt<=days;
+    }
     
     int shipWithinDays(vector<int>& weights, int days) {
         
-        int ans=0;
-        
         int low=*max_element(weights.begin(),weights.end());
-        int high=accumulate(weights.begin(),weights.end(),0);
+        int high= accumulate(weights.begin(),weights.end(),0);
+        
+        int ans=0;
         
         while(low<=high){
             
-            int mid_weight=low+(high-low)/2;
+            int mid_weight= low+(high-low)/2;
             
-            if(isValid(weights,days,mid_weight)){
-                
+            if(isValid(mid_weight,weights,days)){
                 ans=mid_weight;
                 high=mid_weight-1;
             }
             
             else{
-                
                 low=mid_weight+1;
             }
-            
-            
         }
         
         return ans;
-        
     }
 };
