2446074ef6736eb354b0398e098d01427c422af0
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-24 00:01:35 +0530

    Time: 3 ms (45.51%), Space: 5.9 MB (57.35%) - LeetHub

--- 0991-broken-calculator/0991-broken-calculator.cpp
+++ 0991-broken-calculator/0991-broken-calculator.cpp
@@ -3,17 +3,21 @@
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
+        
         int cnt=0;
+        
         while(target>startValue){
             
-            if(target%2==0) target/=2;
+            if(target%2==0){
+                target/=2;
+                cnt++;
+            }
             
             else{
-                target++;
+                target=target+1;
+                cnt++;
             }
-            cnt++;
         }
-        
         
         return cnt+(startValue-target);
         
