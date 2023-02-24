1686b6f81f8ff19951e1437b87756865b4433cfd
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-24 17:44:22 +0530

    Time: 690 ms (37.25%), Space: 72.3 MB (48.98%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int> lks;
         long long ans=0;
         for(int x : a){
-            for(int i=1;i<=(1<<29);i*=2){
+            for(long i=1;i<=(1<<30);i*=2){
                 if(lks.count(i-x)) ans+=lks[i-x];
             }
             lks[x]+=1;
