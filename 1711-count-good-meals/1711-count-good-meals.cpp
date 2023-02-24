02bce061df7b8312f56b89394e1f21295eed47dc
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-24 19:08:51 +0530

    Time: 793 ms (31.89%), Space: 72.3 MB (42.60%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-   int countPairs(vector<int>& arr) {
-        unordered_map<int,int> freq;
-        long long ans=0;
-        for(int i=0;i<arr.size();i++){
-            for(int pow2=1;pow2<=(1<<21);pow2*=2){
-                if(freq.count(pow2-arr[i])) ans+=freq[pow2-arr[i]];
-            }
-            freq[arr[i]]+=1;
-        }
-        return ans % (int)(1e9 + 7);
+    int countPairs(vector<int>& arr) {
+           int n = arr.size();
+    unordered_map<int, int> freq;
+    for (int i = 0; i < n; i++) {
+        freq[arr[i]]++;
     }
-
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        freq[arr[i]]--;
+        for (int j = 0; j <= 30; j++) {
+            int target = (1 << j) - arr[i];
+            if (freq.count(target)) {
+                ans = (ans + freq[target]) % int (1e9+7);
+            }
+        }
+    }
+    return ans;
+    }
 };
