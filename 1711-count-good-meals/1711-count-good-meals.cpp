347ee4f7318c2138afb2db1f01bba18266e2a6da
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-24 18:10:40 +0530

    Time: 735 ms (33.42%), Space: 72.4 MB (42.60%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int> freq;
         long long ans=0;
         for(int i=0;i<arr.size();i++){
-            for(int pow2=1;pow2<=(1<<21);pow2*=2){
+            for(long pow2=1;pow2<=(1ll<<31);pow2*=2){
                 if(freq.count(pow2-arr[i])) ans+=freq[pow2-arr[i]];
             }
             freq[arr[i]]+=1;
