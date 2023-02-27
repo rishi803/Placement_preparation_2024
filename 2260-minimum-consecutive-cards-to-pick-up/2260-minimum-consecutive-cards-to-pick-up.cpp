ade7baae73a1ab9ee833f7b99c7ebf31f7beb17b
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-02-27 19:18:03 +0530

    Time: 361 ms (54.96%), Space: 115.1 MB (89.65%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -3,17 +3,21 @@ public:
     int minimumCardPickup(vector<int>& cards) {
         
         unordered_map<int,int>mp;
-        int head=0,tail=0,mnAns=INT_MAX;
+        
+        int head=0,tail=0,minans=INT_MAX;
         
         for(head=0;head<cards.size();head++){
+            
             mp[cards[head]]++;
             
             while(mp[cards[head]]>1){
-                mnAns=min(mnAns,head-tail+1);
+                minans=min(minans,head-tail+1);
                 mp[cards[tail]]--;
                 tail++;
             }
         }
-        return mnAns==INT_MAX?-1:mnAns;
+        
+        return minans==INT_MAX?-1:minans;
+      
     }
 };
