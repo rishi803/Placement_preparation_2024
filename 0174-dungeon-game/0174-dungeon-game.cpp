6b0d36a4a4536fc362c0eb34fcabb55666706833
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-03-01 19:23:05 +0530

    Time: 4 ms (88.00%), Space: 8.8 MB (79.00%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -4,12 +4,12 @@ public:
     vector<vector<int>>dp;
     
     int help(vector<vector<int>>&dungeon,int row,int col,int i,int j){
-        if(i>=row or j>=col) return INT_MIN+100;
+        if(i>=row or j>=col) return INT_MIN+1000;
         if(dp[i][j]!=-1) return dp[i][j];
         if(i==row-1 and j==col-1) return dungeon[i][j]>=0?0:dungeon[i][j];
             
-        long long right=dungeon[i][j]+help(dungeon,row,col,i,j+1);
-        long long down=dungeon[i][j]+help(dungeon,row,col,i+1,j);
+         int right=dungeon[i][j]+help(dungeon,row,col,i,j+1);
+         int down=dungeon[i][j]+help(dungeon,row,col,i+1,j);
         
        int minhealth=max(right,down);
         if(minhealth>=0) minhealth=0;
