1815762d85dbe892bf02b9e29aff51a1b22afe32
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-03-03 00:18:24 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (12.46%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -8,10 +8,10 @@ public:
         ListNode* prev=dummy;
         ListNode* curr=head;
         
-        while(curr and curr->next){
-            prev->next=curr->next;
-            curr->next=curr->next->next;
-            prev->next->next=curr;
+        while(curr and curr->next){     // (0) 1---->2----->3----->4   cur=1 ,prev=0
+            prev->next=curr->next;      //  (0)--->2      1---->2---->3----->4  
+            curr->next=curr->next->next; // (0)--->2      1---->3---->4    cur->next=2 , cur->next->next=3
+            prev->next->next=curr;       // (0)--->2---->1--->3--->4     
             
             prev=curr;
             curr=curr->next;
