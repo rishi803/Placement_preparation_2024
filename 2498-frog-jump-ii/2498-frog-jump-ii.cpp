a506826e6089c545329141f3f1cdff5e999475e0
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-03-08 19:12:53 +0530

    Time: 139 ms (92.67%), Space: 65.4 MB (69.97%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,3 +1,5 @@
+// already sorted so find max of odd path and even path elements
+
 class Solution {
 public:
     int maxJump(vector<int>& stones) {
@@ -7,11 +9,11 @@ public:
         // for(int i = 2; i < stones.size(); i+=2) res = max(res, stones[i]-stones[i-2]); // even path
         // return res;
         
-        if(stones.size()==2) return stones[1]-stones[0];
+        if(stones.size()==2) return stones[1]-stones[0];   // 0 3
         
          int ans=0;
         
-        for(int i=2;i<stones.size();i++){
+        for(int i=2;i<stones.size();i++){    // 0 2 5 6 7  so==>(0,5,7) and (6,2)
             ans=max(ans,stones[i]-stones[i-2]);           
         }
         
