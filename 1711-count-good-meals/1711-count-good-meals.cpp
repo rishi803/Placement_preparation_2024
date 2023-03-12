bce8543e9e392d512658f6d3ac4c458bfd0cdb4e
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-03-12 19:13:27 +0530

    Time: 549 ms (64.00%), Space: 72.4 MB (43.69%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     int countPairs(vector<int>& arr) {
-           int n = arr.size();
+    //        int n = arr.size();
+    // unordered_map<int, int> freq;
+    // for (int i = 0; i < n; i++) {
+    //     freq[arr[i]]++;
+    // }
+    // int ans = 0;
+    // for (int i = 0; i < n; i++) {
+    //     freq[arr[i]]--;
+    //     for (int j = 0; j <= 30; j++) {
+    //         int target = (1 << j) - arr[i];
+    //         if (freq.count(target)) {
+    //             ans = (ans + freq[target]) % int (1e9+7);
+    //         }
+    //     }
+    // }
+    // return ans;
+        
+               int n = arr.size();
     unordered_map<int, int> freq;
-    for (int i = 0; i < n; i++) {
-        freq[arr[i]]++;
-    }
+    freq[arr[0]]=1;
     int ans = 0;
-    for (int i = 0; i < n; i++) {
-        freq[arr[i]]--;
-        for (int j = 0; j <= 30; j++) {
+    for (int i = 1; i < n; i++) {
+       
+        for (int j = 0; j <= 21; j++) {
             int target = (1 << j) - arr[i];
             if (freq.count(target)) {
                 ans = (ans + freq[target]) % int (1e9+7);
             }
         }
+         freq[arr[i]]++;
     }
     return ans;
     }
