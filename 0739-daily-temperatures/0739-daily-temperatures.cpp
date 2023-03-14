90ab82d31fff00f0986350fa94a8a69d899b9ac7
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-03-14 19:35:49 +0530

    Time: 235 ms (25.26%), Space: 103.1 MB (34.00%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -4,7 +4,9 @@ public:
         stack<int> s;
         vector<int> ans(temp.size());
         for(int cur = 0; cur < temp.size(); cur++) {
+            
             while(s.size() and temp[cur] > temp[s.top()]) {    // pop till current temp < stack's top's temp. This maintains monotonic stack
+                // cout<<s.top()<<endl;
                 ans[s.top()] = cur - s.top();           // cur day will be next warmer day for each element that's popped
                 s.pop();
             }
