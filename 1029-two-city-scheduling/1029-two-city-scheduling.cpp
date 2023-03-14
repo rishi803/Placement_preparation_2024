a14d36b171b0bdd8c33900f7c20ed1b8efb11dde
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-03-15 00:46:32 +0530

    Time: 7 ms (44.24%), Space: 8.4 MB (20.30%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -7,12 +7,12 @@ class Solution {
  int findMinValUsingDP(vector<vector<int>>& costs,int X,int Y,int T,vector<vector<int>>&dp){
         if(costs.size()==T)return 0;
         if(dp[X][Y]!=INT_MAX)return dp[X][Y];
-        int minVal = INT_MAX;
+        int costx=INT_MAX,costy=INT_MAX;
         if(X)
-            minVal = costs[T][0]+findMinValUsingDP(costs,X-1,Y,T+1,dp);
+            costx= costs[T][0]+findMinValUsingDP(costs,X-1,Y,T+1,dp);
         if(Y)
-            minVal = min(minVal,costs[T][1]+findMinValUsingDP(costs,X,Y-1,T+1,dp));
-        return dp [X][Y] = minVal;
+            costy = costs[T][1]+findMinValUsingDP(costs,X,Y-1,T+1,dp);
+        return dp [X][Y] = min(costx,costy);
     }
     int twoCitySchedCost(vector<vector<int>>& costs){
         vector<vector<int>>dp(costs.size()/2+1,vector<int>(costs.size()/2+1,INT_MAX));
