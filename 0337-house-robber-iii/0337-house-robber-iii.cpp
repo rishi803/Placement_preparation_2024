1b11c72cba1cadfca3b598ce122b4fbc62c8d6bd
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-03-15 19:36:01 +0530

    Time: 68 ms (5.50%), Space: 28.4 MB (23.02%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -1,53 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 
-// TEST CASE: [2,1,3,null,4],       [4,1,null,2,null,]
 class Solution {
 public:
+       unordered_map<TreeNode*,vector<int>>dp;   
     
-//     pair<int,int> help(TreeNode* root){
+    int help(TreeNode* root,bool flag){
         
-//         if(!root) return {0,0};                        // pair(include,exclude)
-//         pair<int,int>leftans=help(root->left);
-//         pair<int,int>rightans=help(root->right);
-        
-//         int include_root=root->val+leftans.second+rightans.second;
-//         int exclude_root=max(leftans.first,leftans.second)+max(rightans.first,rightans.second);
-        
-//         return {include_root,exclude_root};
-//     }
-    
-//     int rob(TreeNode* root) {
-//         pair<int,int>ans=help(root);
-//         return max(ans.first,ans.second);
-//     }
-    
-    unordered_map<TreeNode*,vector<int>>dp;      
-    
-    int help(TreeNode* root,bool canrob){
         if(!root) return 0;
-        if(dp.count(root) && dp[root][canrob] != -1) return dp[root][canrob];
+        if(dp.count(root) && dp[root][flag] != -1) return dp[root][flag];
         dp[root] = {-1,-1};
-        int takeroot=0;
-            if(canrob)
-            takeroot=root -> val + help(root -> left, false) + help(root -> right, false);
         
-        int notroot= 0;
-          notroot=help(root->left,true)+help(root->right,true);
+        int canrob=INT_MIN,notrob=INT_MIN;
         
-        return dp[root][canrob]=max(takeroot,notroot);
+        if(flag) {
+             canrob=root->val+help(root->left,false)+help(root->right,false);
+              notrob=help(root->left,true)+help(root->right,true);
+        }
+        
+        else{
+            notrob=help(root->left,true)+help(root->right,true);
+        }
+        
+        return dp[root][flag]=max(canrob,notrob);
     }
+    
     int rob(TreeNode* root) {
-        return help(root,1);
-      
+        
+        return help(root,true);
     }
 };
