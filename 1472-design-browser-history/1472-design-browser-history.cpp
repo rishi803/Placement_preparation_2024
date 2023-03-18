30e36d6ba3a4b3ca57c5a35ace5a0b661f73a94c
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-03-18 12:44:08 +0530

    Time: 143 ms (68.95%), Space: 57 MB (90.15%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -8,17 +8,18 @@ public:
     }
     
     void visit(string url) {
-        v[++count]=url;
+        v[++count]=url;          // ex- [ google , facebook , yahoo ] if we are at facebook and visited
+                                // linkedin then scenario will change to [ google, facebook, linkedin ]
         curr=count;
     }
     
     string back(int steps) {
-        count = max(count-steps, 0);
+        count = max(count-steps, 0);    // count shows at which position we are currently
         return v[count];
     }
     
     string forward(int steps) {
-        count = min(curr, count+steps);
+        count = min(curr, count+steps);    // curr shows that how many sites we have visited
         return v[count];
     }
 };
