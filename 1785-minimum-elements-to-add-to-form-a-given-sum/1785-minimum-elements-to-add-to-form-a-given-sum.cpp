14ce99c5d05caadd7c164f9a312fa40d68babeac
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-03-29 10:22:54 +0530

    Time: 133 ms (50.88%), Space: 73.5 MB (6.58%) - LeetHub

--- 1785-minimum-elements-to-add-to-form-a-given-sum/1785-minimum-elements-to-add-to-form-a-given-sum.cpp
+++ 1785-minimum-elements-to-add-to-form-a-given-sum/1785-minimum-elements-to-add-to-form-a-given-sum.cpp
@@ -4,10 +4,12 @@ public:
    
     int minElements(vector<int>& nums, int limit, int goal) {
         
-        if(limit==300 and goal==6 and nums.size()==3 and nums[0]==1 and nums[1]==2 and nums[2]==3) return 0;
+       
         long long sum=0;
         
         for(auto i:nums) sum+=i;
+        
+        if(sum==goal) return 0;
          long long req=goal-sum;
          
         
