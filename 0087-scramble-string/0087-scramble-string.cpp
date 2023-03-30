1138a5da6486cf3d9e9c7e74e7240f558700b81c
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-03-31 01:31:12 +0530

    Time: 201 ms (28.46%), Space: 40.7 MB (23.93%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,3 +1,10 @@
+//     s1= great      s2=  rgeat
+//     if(notswap) check isScramble(s1.substr(0,i),s2.substr(0,i)) and isScramble(s1.substr(i,n-i),s2.substr(i,n-i)); i.e ("gr" of s1,"rg" of s2)  and ("eat" of s1,"eat" of s2)
+        
+//     if(swapped)  check isScramble(s1.substr(0,i),s2.substr(n-i,i)) and isScramble(s1.substr(i,n-i),s2.substr(0,n-i));  i.e ("gr" of s1, "at" of s2) and ("eat" of s1,"rge" of s2)
+
+// above example is considered for i=2
+
 class Solution {
 public:
     
@@ -7,30 +14,30 @@ public:
     bool isScramble(string s1, string s2) {
         
         if(s1==s2) return true;
-        if(s1.size()!=s2.size()) return false;
+        if(s1.size()!=s2.size()) return false;   // if size of both string not equal it will never scrambled
         
-        string key=s1+" "+s2;
+        string key=s1+" "+s2;          // to distinguish (ex- great with gr|eat as gr_eat(x+y) and eat_gr(y+x))
         if(mp.find(key)!=mp.end()) return mp[key];
         
         
-        bool isswap=false,swapped=false,result=false;
+        bool notswap=false,swapped=false;
         int n=s1.size();
         
         for(int i=1;i<n;i++){
             
-            isswap= isScramble(s1.substr(0,i),s2.substr(0,i)) and isScramble(s1.substr(i,n-i),s2.substr(i,n-i));
+            notswap= isScramble(s1.substr(0,i),s2.substr(0,i)) and isScramble(s1.substr(i,n-i),s2.substr(i,n-i));
             
-            if(isswap) return true;
+            if(notswap) return true;     // agar bina swap kiye hi s1 aur s2 match kr gaya
             
             swapped= isScramble(s1.substr(i,n-i),s2.substr(0,n-i)) and isScramble(s1.substr(0,i),s2.substr(n-i,i));
             
-            if(swapped) return true;
+            if(swapped) return true;     // agar swap (y+x) krne pr s1 aur s2 match kr gaye
             
              
         }
         
        
         
-        return mp[key]=false;
+        return mp[key]=false;       // store that they can not be equal for this particular string
     }
 };
