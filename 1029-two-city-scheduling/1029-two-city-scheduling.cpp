b0f5961124af214e22f6ae25afe02f50f98707bd
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-02 18:22:56 +0530

    Time: 7 ms (43.22%), Space: 8.4 MB (21.83%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     
-    int help(vector<vector<int>>&costs,int cangoA,int cangoB,int idx,vector<vector<vector<int>>>&dp){
+    int help(vector<vector<int>>&costs,int cangoA,int cangoB,int idx,vector<vector<int>>&dp){
         if(idx==costs.size()) return 0;
-        if(dp[cangoA][cangoB][idx]!=-1) return dp[cangoA][cangoB][idx];
+        if(dp[cangoA][cangoB]!=-1) return dp[cangoA][cangoB];
         
         int cost1=INT_MAX,cost2=INT_MAX;
         
@@ -11,11 +11,11 @@ public:
         
         if(cangoB) cost2=costs[idx][1]+help(costs,cangoA,cangoB-1,idx+1,dp);
         
-        return dp[cangoA][cangoB][idx]=min(cost1,cost2);
+        return dp[cangoA][cangoB]=min(cost1,cost2);
     }
     int twoCitySchedCost(vector<vector<int>>& costs) {
         
-        vector<vector<vector<int>>>dp(costs.size()/2+1,vector<vector<int>>(costs.size()/2+1,vector<int>(costs.size(),-1)));
+        vector<vector<int>>dp(costs.size()/2+1,vector<int>(costs.size()/2+1,-1));
          
         return help(costs,costs.size()/2,costs.size()/2,0,dp);
     }
