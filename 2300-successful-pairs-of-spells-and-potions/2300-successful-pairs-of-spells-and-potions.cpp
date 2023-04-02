457805f7c912e862ad34b6d2004e9ad2e859332a
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-03 00:33:30 +0530

    Time: 255 ms (57.84%), Space: 100 MB (31.79%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,11 +1,31 @@
 class Solution {
 public:
+    
+    int lowerbound(long long target,vector<int>&nums,int l,int h){
+        
+        
+        
+        while(l<=h){
+         int mid=l+(h-l)/2;
+         
+          if(nums[mid]>=target){
+              h=mid-1;
+          }
+         else if(nums[mid]<target){
+             l=mid+1;
+         }
+        }
+        return l;
+        
+       
+        
+    }
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         vector<int>ans;
         sort(potions.begin(),potions.end());
         for(int i=0;i<spells.size();i++){
             long long target=((success-1)/spells[i])+1;
-            int lb=lower_bound(potions.begin(),potions.end(),target)-potions.begin();
+            int lb=lowerbound(target,potions,0,potions.size()-1);
             ans.push_back(potions.size()-lb);
         }
         return ans;
