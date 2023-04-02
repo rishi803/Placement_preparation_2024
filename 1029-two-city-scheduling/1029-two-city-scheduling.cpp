ed453d0d1f69a7b6e994d25bb0b69a20e6c0dbc3
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-02 18:18:11 +0530

    Time: 3 ms (86.61%), Space: 8.4 MB (19.59%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,21 +1,22 @@
-// REAL LIFE APPLICATION
-
 class Solution {
+public:
     
-    public: 
-    
- int findMinValUsingDP(vector<vector<int>>& costs,int cityA,int cityB, int T,vector<vector<int>>&dp){
-        if(costs.size()==T)return 0;
-        if(dp[cityA][cityB]!=INT_MAX)return dp[cityA][cityB];
-        int costx=INT_MAX,costy=INT_MAX;
-        if(cityA)
-            costx= costs[T][0]+findMinValUsingDP(costs,cityA-1,cityB,T+1,dp);
-        if(cityB)
-            costy = costs[T][1]+findMinValUsingDP(costs,cityA,cityB-1,T+1,dp);
-        return dp [cityA][cityB] = min(costx,costy);
+    int help(vector<vector<int>>&costs,int cangoA,int cangoB,int idx,vector<vector<int>>&dp){
+        if(idx==costs.size()) return 0;
+        if(dp[cangoA][cangoB]!=-1) return dp[cangoA][cangoB];
+        
+        int cost1=INT_MAX,cost2=INT_MAX;
+        
+        if(cangoA) cost1= costs[idx][0]+help(costs,cangoA-1,cangoB,idx+1,dp);
+        
+        if(cangoB) cost2=costs[idx][1]+help(costs,cangoA,cangoB-1,idx+1,dp);
+        
+        return dp[cangoA][cangoB]=min(cost1,cost2);
     }
-    int twoCitySchedCost(vector<vector<int>>& costs){
-        vector<vector<int>>dp(costs.size()/2+1,vector<int>(costs.size()/2+1,INT_MAX));
-        return findMinValUsingDP(costs,costs.size()/2,costs.size()/2,0,dp);
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        
+        vector<vector<int>>dp(costs.size()/2+1,vector<int>(costs.size()/2+1,-1));
+         
+        return help(costs,costs.size()/2,costs.size()/2,0,dp);
     }
 };
