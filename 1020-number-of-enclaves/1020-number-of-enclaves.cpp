cca51ffb84a07bbc8a79bb0e5273c574f76fa5e1
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-07 11:55:00 +0530

    Time: 66 ms (95.06%), Space: 30.2 MB (51.97%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,27 +1,38 @@
+// easier one (call dfs/bfs from boundary where there is 1 in 4 direction having 1)
+
 class Solution {
 public:
     
     void help(int row,int col,int i,int j,vector<vector<int>>&grid){
         
-        queue<pair<int,int>>q;
-        vector<int>dir={-1,0,1,0,-1};
-        q.push({i,j});
+//         queue<pair<int,int>>q;
+//         vector<int>dir={-1,0,1,0,-1};
+//         q.push({i,j});
         
-        while(!q.empty()){
-            auto [x,y]=q.front();
-            q.pop();
-            grid[x][y]=0;
+//         while(!q.empty()){
+//             auto [x,y]=q.front();
+//             q.pop();
+//             grid[x][y]=0;
             
-            for(int k=0;k<4;k++){
-                int newx=x+dir[k];
-                int newy=y+dir[k+1];
+//             for(int k=0;k<4;k++){
+//                 int newx=x+dir[k];
+//                 int newy=y+dir[k+1];
                 
-                if(newx<row and newy<col and newx>=0 and newy>=0 and grid[newx][newy]==1){
-                    grid[newx][newy]=0;
-                    q.push({newx,newy});
-                }
-            }
-        }
+//                 if(newx<row and newy<col and newx>=0 and newy>=0 and grid[newx][newy]==1){
+//                     grid[newx][newy]=0;
+//                     q.push({newx,newy});
+//                 }
+//             }
+//         }
+        
+         if(i>=row or j>=col or i<0 or j<0 or grid[i][j]==0) return ;
+        
+        grid[i][j]=0;
+        
+        help(row,col,i+1,j,grid);
+        help(row,col,i,j+1,grid);
+        help(row,col,i-1,j,grid);
+        help(row,col,i,j-1,grid);
     }
     int numEnclaves(vector<vector<int>>& grid) {
         
