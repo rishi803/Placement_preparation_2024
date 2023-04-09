7292ffa234adb03ccb73887003cba349888504e2
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-09 22:05:53 +0530

    Time: 206 ms (25.00%), Space: 81.2 MB (25.00%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    bool isvalid(int canans,vector<int>&nums,int p){
+        
+        int cnt=0;
+        
+        for(int i=1;i<nums.size();i++){
+            
+           if(nums[i]-nums[i-1]<=canans) {
+               // if(canans==0) cout<<nums[i]<<" b "<<nums[i-1]<<endl;
+               cnt++;
+               i++;
+           }
+        
+            
+        }
+        // cout<<cnt<<endl;
+        
+        return cnt>=p;
+        
+    }
+    int minimizeMax(vector<int>& nums, int p) {
+        int l=0;
+        int h=*max_element(nums.begin(),nums.end());
+        int ans=0;
+     
+        sort(nums.begin(),nums.end());
+        
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            
+            if(isvalid(mid,nums,p)){
+                // cout<<" m= "<<mid<<endl;
+                ans=mid;
+                h=mid-1;
+            }
+            
+            else{
+                l=mid+1;
+            }
+        }
+        
+        return ans;
+    }
+};
