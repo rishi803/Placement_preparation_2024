c6c25edf4e3a9e82501b82206797eadb4c9b8364
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-12 18:14:08 +0530

    Time: 3 ms (63.65%), Space: 11.6 MB (49.16%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
    bool haveConflict(vector<string>& event1, vector<string>& event2) {
-        if(event2[0] <= event1[1] && event2[1] >= event1[0]) return true;
-        return false;
+       
+       if(event1[1]<event2[0] and event1[0]<event2[1]) return false;
+       else if(event1[1]>event2[0] and event1[0]>event2[1]) return false;
+       else return true;
     }
 };
