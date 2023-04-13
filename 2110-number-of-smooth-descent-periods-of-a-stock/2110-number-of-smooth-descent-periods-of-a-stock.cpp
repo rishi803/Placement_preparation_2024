d0ac6da9856ba9e6e82fc1326973e0707b0218d2
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-13 18:55:16 +0530

    Time: 168 ms (76.56%), Space: 103 MB (72.37%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -1,12 +1,16 @@
+// why not head-tail+1  (because i will directly add size at last all single elements)
+// during execution if i add single occurence then how i know that which single element remains thats why
+// this some good sliding window problems in which you have to apply by yourself
+
 class Solution {
 public:
     long long getDescentPeriods(vector<int>& nums) {
         
         long long head=0,tail=0,ans=0;
         
-        for(head=0;head<nums.size();head++){
+        for(head=1;head<nums.size();head++){
             
-            while(head>0 and head<nums.size() and nums[head]-nums[head-1]==-1)  ans+=(head-tail),head++;
+            while(head<nums.size() and nums[head]-nums[head-1]==-1)  ans+=(head-tail),head++;
             
             tail=head;
         }
