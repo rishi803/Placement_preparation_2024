4a64ba9c6a23a827e7b0470082111829ad40fffc
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-15 03:11:56 +0530

    Time: 2601 ms (16.83%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,12 @@
 # Write your MySQL query statement below
+
+ # using left join (left join joins two table and for primary key of table 1 is not in table 2 show null )
+ 
+# select  empuni.unique_id,emp.name
+# from Employees as emp left join EmployeeUNI as empuni
+# on emp.id=empuni.id
+
+# using right join (replace table1 as table2 and vice versa)
 select  empuni.unique_id,emp.name
-from Employees as emp left join EmployeeUNI as empuni
-on emp.id=empuni.id
+from  EmployeeUNI as empuni right join Employees as emp
+on empuni.id=emp.id
