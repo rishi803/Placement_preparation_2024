61dabce8c0b16a851bde4b012694df2722ef1331
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-16 23:52:52 +0530

    Time: 438 ms (67.19%), Space: 0B (100.00%) - LeetHub

---
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -0,0 +1,10 @@
+# IF (sex = 'm', 'f', 'm')
+
+# This will check whether sex is 'm':
+# If yes then it will set SEX as 'f'
+# else it will set it as 'm' [meaning when SEX = 'f']
+
+update Salary 
+set sex=IF (sex = 'm', 'f', 'm')
+
+
