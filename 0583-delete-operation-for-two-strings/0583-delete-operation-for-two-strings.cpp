31921703cdc361d714def9619f8273b13e88041c
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-17 02:13:28 +0530

    Time: 24 ms (58.30%), Space: 12.2 MB (59.00%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    char dp[502][502];
-    int help(int id1,int id2,string word1,string word2){
-     
-        if(id1==0) return id2;
-        if(id2==0) return id1;
-        // if(id1>=word1.size() or id2>=word2.size()) return INT_MAX-100;
-        if(dp[id1][id2]!=-1) return dp[id1][id2];
-        int option1=INT_MAX,option2=INT_MAX;
-        
-        if(word1[id1-1]!=word2[id2-1])
-            option1=1+min(help(id1-1,id2,word1,word2),help(id1,id2-1,word1,word2));
-        else
-            option2= help(id1-1,id2-1,word1,word2);
-        
-        return dp[id1][id2]=min(option1,option2);
+    int help(string& word1,int n,string& word2,int m,vector<vector<int>>&dp){
+        if(n==0) return m;
+        if(m==0) return n;
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(word1[n-1]==word2[m-1]){
+            return dp[m][n]= help(word1,n-1,word2,m-1,dp);
+        }
+        else{
+            return dp[m][n]= 1+min(help(word1,n-1,word2,m,dp),help(word1,n,word2,m-1,dp));
+        }
     }
     
     int minDistance(string word1, string word2) {
-        memset(dp,-1,sizeof(dp));
-        return abs(help(word1.size(),word2.size(),word1,word2));
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return help(word1,n,word2,m,dp);
     }
 };
