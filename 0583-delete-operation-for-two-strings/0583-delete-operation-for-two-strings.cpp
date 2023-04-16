df37a3e95b7359240394f6dfdd0c712382db6cd5
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-17 02:11:13 +0530

    Time: 949 ms (5.06%), Space: 375.8 MB (7.57%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[502][502];
+    int help(int id1,int id2,string word1,string word2){
+     
+        if(id1==0) return id2;
+        if(id2==0) return id1;
+        // if(id1>=word1.size() or id2>=word2.size()) return INT_MAX-100;
+        if(dp[id1][id2]!=-1) return dp[id1][id2];
+        int option1=INT_MAX,option2=INT_MAX;
+        
+        if(word1[id1-1]!=word2[id2-1])
+            option1=1+min(help(id1-1,id2,word1,word2),help(id1,id2-1,word1,word2));
+        else
+            option2= help(id1-1,id2-1,word1,word2);
+        
+        return dp[id1][id2]=min(option1,option2);
+    }
+    
+    int minDistance(string word1, string word2) {
+        memset(dp,-1,sizeof(dp));
+        return help(word1.size(),word2.size(),word1,word2);
+    }
+};
