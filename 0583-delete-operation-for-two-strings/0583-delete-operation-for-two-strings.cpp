00da25d91791cbddf39df058de60b56410daf73f
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-17 02:23:48 +0530

    Time: 122 ms (12.56%), Space: 7.4 MB (86.53%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int dp[502][502];
     int help(string& word1,int n,string& word2,int m){
-        if(n==word1.size()) return word2.size()-m;
-        if(m==word2.size()) return word1.size()-n;
+        if(n==0) return m;
+        if(m==0) return n;
         if(dp[m][n]!=-1) return dp[m][n];
         int op1=INT_MAX,op2=INT_MAX;
         
-        if(word1[n]==word2[m]){
-            op1= help(word1,n+1,word2,m+1);
+        if(word1[n-1]==word2[m-1]){
+            op1= help(word1,n-1,word2,m-1);
         }
         else{
-            op2= 1+min(help(word1,n+1,word2,m),help(word1,n,word2,m+1));
+            op2= 1+min(help(word1,n-1,word2,m),help(word1,n,word2,m-1));
         }
         
         return dp[m][n]=min(op1,op2);
@@ -22,6 +22,6 @@ public:
         int n=word1.size();
         int m=word2.size();
         // vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return help(word1,0,word2,0);
+        return help(word1,n,word2,m);
     }
 };
