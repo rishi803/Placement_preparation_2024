8f9595e2e81dfc3abf0be2a1d9da9e3f278a9894
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-17 00:13:00 +0530

    Time: 581 ms (13.32%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,10 +1,17 @@
 # IF (sex = 'm', 'f', 'm')
-
 # This will check whether sex is 'm':
 # If yes then it will set SEX as 'f'
 # else it will set it as 'm' [meaning when SEX = 'f']
 
-update Salary 
-set sex=IF (sex = 'm', 'f', 'm')
+# update Salary 
+# set sex=IF (sex = 'm', 'f', 'm')
+
+
+# REPLACE(string, old_string, new_string)
+# string = 'fm' # row01 = 'f'. So, sex = 'f'. We'll now find 'f' in 'fm' and will replace it with ''. So, the result is 'm'.
+# row02 = 'm'. So, sex = 'm'. We'll now find 'm' in 'fm' and will replace it with ''. So, the result is 'f'.
+
+UPDATE salary 
+SET sex = REPLACE ('ffm', sex, '');
 
 
