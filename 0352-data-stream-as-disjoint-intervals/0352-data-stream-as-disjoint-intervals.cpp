1aebdbddc654d634f8da427e425ca30f031f461f
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-17 19:30:14 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (96.82%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -1,33 +1,35 @@
 class SummaryRanges {
 public:
    
-set<int> nums;
+    set<int>st;
+    
     SummaryRanges() {
-        
+       
     }
     
     void addNum(int value) {
-        nums.insert(value);
+      st.insert(value);
     }
     
     vector<vector<int>> getIntervals() {
-      vector<vector<int>> intervals;
-        int start = -1;
-        int end = -1;
-        for (auto num : nums) {
-            if(end < 0)
-                start = end = num;
-            else if (num - end == 1) {
-                end = num;
-            } else {
-                intervals.push_back({start, end});
-                start = end = num;
-            }
-        }
-        if(start!=-1 and end!=-1)
-        intervals.push_back({start, end});
-        return intervals;
+        vector<vector<int>>ans;
+        if(st.empty()) return {};
+        int start=*st.begin(),end=*st.begin();
+        
+          for(auto it=next(st.begin());it!=st.end();it++){
+              int val=*it;
+              if((val-end==1)){
+                  end=val;
+              }
+              
+              else{
+                  ans.push_back({start,end});
+                  start=val,end=val;
+              }
+          }
 
+        ans.push_back({start,end});
+        return ans;
     }
 };
 
