79711464d71dff6c0b430f3a2476678f674fb830
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-22 14:32:36 +0530

    Time: 474 ms (73.30%), Space: 186 MB (78.53%) - LeetHub

--- 2276-count-integers-in-intervals/2276-count-integers-in-intervals.cpp
+++ 2276-count-integers-in-intervals/2276-count-integers-in-intervals.cpp
@@ -9,7 +9,7 @@ public:
     void add(int left, int right) {
         auto it = st.lower_bound({left, right});
         
-        if(it != st.begin() && (--it)->second < left) {
+        if(it != st.begin() && (--it)->second < left) { // left,right jha pr insert hoga vaha agr vo beginning h to koi na uska age vale elements se check krenge overlap lkin agr beginning ke bad ka koi h to just piche vale se overlap check krenge kunki baki piche ke elements bina overlap ke hi insert kiye hue h ( ab doubt a skta h to ki agr--> (1,2) (3,8) (16,19) (20,25) me (4,30) insert krna h to dhyan do lower bound vhi position dega jha iski phli occurence honi chahiye mtlb (3,8) ke bad aur isi se overlap ki probability ho skti hai bas lkin age ye sare element se overlap check krega jo niche kiya gaya h)
             ++it;
         }
         
