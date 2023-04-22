3c8fa8a69ca284720b6e336db3bafed42033e833
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-22 23:16:28 +0530

    Time: 346 ms (10.41%), Space: 7.6 MB (19.44%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
+    
+    // int tsum;
+    
     int dp[1001][51];
-    int solve(vector<int>& nums,int idx,int k)
-    {
-        if(idx==nums.size() && k==0)
-        {
-            return 0;
+    
+    int help(vector<int>&nums,int k,int idx){
+         
+        if(k==0 and idx==nums.size()) return 0;
+        if(idx==nums.size() or k==0) return INT_MAX;
+        if(dp[idx][k]!=-1) return dp[idx][k];
+        
+        int mx=INT_MAX,sum=0;
+        
+        for(int i=idx;i<nums.size();i++){
+            mx=min(mx,max(sum+=nums[i],help(nums,k-1,i+1)));
         }
-        if(idx==nums.size() || k==0)
-            return INT_MAX;
-        if(dp[idx][k]!=-1)
-            return dp[idx][k];
-        int res=INT_MAX,cnt=0;
-        for(int i=idx;i<nums.size();i++)
-        {
-            cnt+=nums[i];
-            int p=solve(nums,i+1,k-1);
-            int ans=max(cnt,p);
-            res=min(res,ans);
-        }
-        return dp[idx][k]=res;
+        
+        return dp[idx][k]=mx;
     }
-    int splitArray(vector<int>& nums, int m) 
-    {
-          memset(dp,-1,sizeof(dp));
-        return solve(nums,0,m);
+    
+    int splitArray(vector<int>& nums, int k) {
+        
+        memset(dp,-1,sizeof(dp));
+        // tsum=accumulate(nums.begin(),nums.end(),0);
+        return help(nums,k,0);
     }
 };
