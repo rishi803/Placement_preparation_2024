76d3b9f1fde8fe74ad67d4af9412c6c06f46a6bd
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-22 12:04:37 +0530

    Time: 2201 ms (27.76%), Space: 15.5 MB (54.76%) - LeetHub

---
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    int dp[71][8000];
+    
+    int help(vector<vector<int>>&mat,int row,int col,int currow,int target,int sum){
+        
+        if(currow>=col) {
+           return abs(sum-target);
+        }
+        if(dp[currow][sum]!=-1) return dp[currow][sum];
+       
+        int mn=INT_MAX;
+        
+        for(int i=0;i<mat[currow].size();i++){
+          
+            mn=min(mn,help(mat,row,col,currow+1,target,sum+mat[currow][i]));
+        }
+        
+        return dp[currow][sum]=mn;
+    }
+    
+    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+        
+       memset(dp,-1,sizeof(dp));
+        int row_h=mat[0].size(),col_v=mat.size();
+       return help(mat,row_h,col_v,0,target,0);
+     
+    }
+};
