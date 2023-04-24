b1533c0ff8fb5a5d32bcb56b5aefefbe4b7c39d7
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-04-24 18:57:52 +0530

    Time: 1083 ms (5.00%), Space: 10.3 MB (73.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // unordered_map<int,int>mp;
+         // unordered_map<int,int>mp;
         // for(int i=0;i<nums.size();i++){
         //     if(mp.find(target-nums[i])!=mp.end()) return{mp[target-nums[i]],i};
         //     mp[nums[i]]=i;
@@ -18,6 +18,6 @@ public:
                 }
             }
         }
-        return output;
+        return output; 
     }
 };
