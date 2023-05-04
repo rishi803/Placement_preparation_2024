543f77e4a3efd56978984fd7604424aeb1453e05
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-05-05 00:17:42 +0530

    Time: 360 ms (66.07%), Space: 141.5 MB (74.87%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -1,4 +1,5 @@
 // memo even gives TLE
+// Approach is: at each index find diff of each of its previous index and check whether curr index with difference occur at any previous index with same difference if so then increment its value by 1 if not then this means that a new subsequence is going to start at this index with difference diff with minimum elment 2 (previous one and current one)
 
 class Solution {
 public:
@@ -10,10 +11,15 @@ public:
     
     for (int i=0; i<n; i++) {
         for (int j=0; j<i; j++) {
-            int diff = nums[i] - nums[j] +500;
-            dp[i][diff] = dp[j][diff] > 0 ? dp[j][diff] + 1 : 2;
-            res = max(res, dp[i][diff]);
+           
+            int diff = nums[i] - nums[j] +500; // to handle -ve differences
+            
+            dp[i][diff] = dp[j][diff] > 0 ? dp[j][diff] + 1 : 2;  // there will be atleast 2 element forming arithmetic (prev one and curr one)
+            
+             // cout<<"j--> "<<"dp["<<j<<"]["<<diff<<"] i--> dp["<<i<<"]["<<diff<<"] = "<<dp[i][diff]<<endl;
+            res = max(res, dp[i][diff]); 
         }
+        // cout<<endl;
     }
     
     return res;
