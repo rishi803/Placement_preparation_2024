dd2cf3fddbf37ea37fa7325fa890869511953f9c
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-05-14 02:38:18 +0530

    Time: 1145 ms (33.33%), Space: 93.7 MB (22.22%) - LeetHub

---
+++ 2680-maximum-or/2680-maximum-or.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    long long maximumOr(vector<int>& nums, int k) {
+         int n = nums.size();
+    vector<long long> pre(n + 1, 0); // Stores prefix bitwise OR values
+    vector<long long> suf(n + 1, 0); // Stores suffix bitwise OR values
+    pre[0] = 0;
+    suf[n] = 0;
+    long long res = 0;
+    long long p = 1; // Used to calculate the power of 2, equivalent to x^k
+    p = p << k; // Left shift k positions to calculate 2^k
+
+    // Calculate prefix bitwise OR values
+    for (int i = 0; i < n; i++) {
+        pre[i + 1] = pre[i] | nums[i];
+    }
+
+    cout<<"Prefix is: ";
+    for(auto i:pre){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+        
+    // Calculate suffix bitwise OR values
+    for (int i = n - 1; i >= 0; i--) {
+        suf[i] = suf[i + 1] | nums[i];
+    }
+        
+   cout<<"Suffix is: ";
+    for(auto i:suf){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    cout<<"P= "<<p<<endl;
+
+    // Find the maximum result by iterating through the numbers
+    for (int i = 0; i < n; i++) {
+        // cout<<"res= "<<res<< " If current num is multiped by 2 k times then answer will be= "<<(pre[i] | (nums[i] * p) | suf[i + 1])<<endl;
+        res = max(res, pre[i] | (nums[i] * p) | suf[i + 1]);
+    }
+
+    return res;
+    }
+};
