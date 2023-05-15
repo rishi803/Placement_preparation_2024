f5cc6ef5b353d4332e6f493c454ac64b26e95a82
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-05-16 03:34:42 +0530

    Time: 1133 ms (5.05%), Space: 328.9 MB (18.04%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     int minSideJumps(vector<int>& obs) {
-    int n=obs.size()-1,step=0;
+    int n=obs.size(),step=0;
     vector<vector<int>> vis(4,vector<int>(n+1,0));
     queue<pair<int,int>> q;
     q.push({2,0});
@@ -13,14 +13,18 @@ public:
             auto [x,y]=q.front();
             q.pop();
             if(x>3||x<1||y>n||y<0||vis[x][y]==1||obs[y]==x) continue;  // eliminate the invalid points 
-            if(y==n) return step-n;  // subract horizontal distance->n
-            vis[x][y]=1;                    // mark visited
+            if(y==n-1) return step-(n-1);  // subract horizontal distance->n
+            vis[x][y]=1;                    // mark visited  
+            
+            if(x-1>=1)
             q.push({x-1,y});              // onr step up  
+            if(x-2>=1)
             q.push({x-2,y});             //two steps up
-            
+            if(x+1<=3)
             q.push({x+1,y});           //one step down
+            if(x+2<=3)
             q.push({x+2,y});           //two steps down 
-            
+            if(y+1<n)
             q.push({x,y+1});         //one step right
             //(if any case is invalid it will get eliminated by if condition)
         }
