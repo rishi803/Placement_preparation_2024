26ce996b2dc4fe06e58c07f332f3d30440617609
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-05-16 03:20:32 +0530

    Time: 1577 ms (5.05%), Space: 349.8 MB (13.87%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,29 +1,32 @@
-//check bfs+dp
-// switching to lane is interesting here (trick with mod value)
+// SIDEBAR--> DP & BFS
 
 class Solution {
 public:
+    int minSideJumps(vector<int>& obs) {
+    int n=obs.size()-1,step=0;
+    vector<vector<int>> vis(4,vector<int>(n+1,0));
+    queue<pair<int,int>> q;
+    q.push({2,0});
+    while(!q.empty()){
+        int sz=q.size();
+        for(int p=0;p<sz;p++){
+            auto [x,y]=q.front();
+            q.pop();
+            if(x>3||x<1||y>n||y<0||vis[x][y]==1||obs[y]==x) continue;  // eliminate the invalid points 
+            if(y==n) return step-n;  // subract horizontal distance->n
+            vis[x][y]=1;                    // mark visited
+            q.push({x-1,y});              // onr step up  
+            q.push({x-2,y});             //two steps up
+            
+            q.push({x+1,y});           //one step down
+            q.push({x+2,y});           //two steps down 
+            
+            q.push({x,y+1});         //one step right
+            //(if any case is invalid it will get eliminated by if condition)
+        }
+        step++;
+    }
     
-    int n;
-
-    int help(int lane,int i,vector<int>&ob,vector<vector<int>>& dp)
-    {
-        
-        if(i==n-1)
-            return 0;
-        if(ob[i]==lane+1)
-            return INT_MAX;
-        if(dp[i][lane]!=-1)
-            return dp[i][lane];
-        if(ob[i+1]!=lane+1)       // 1 step piche hi check krna pdega vrna age bdh jaoge to piche nhi a paoge
-            return dp[i][lane]=help(lane,i+1,ob,dp);
-        else
-        return dp[i][lane]=1+min(help((lane+1)%3,i,ob,dp),help((lane+2)%3,i,ob,dp)); // otherwise lane change and  take minimum of other two lanes
-    }
-    int minSideJumps(vector<int>& obstacles) {
-        n=obstacles.size();
-        vector<vector<int>>dp(n+1,vector<int>(3,-1));
-        return help(1,0,obstacles,dp);
-        
-    }
+    return -1;
+}
 };
