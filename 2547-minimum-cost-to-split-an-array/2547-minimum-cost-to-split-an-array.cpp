d1a6b5384da7396f1004a0ec38f7895cb15a3fcf
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-05-17 02:08:48 +0530

    Time: 392 ms (84.12%), Space: 132.5 MB (78.54%) - LeetHub

---
+++ 2547-minimum-cost-to-split-an-array/2547-minimum-cost-to-split-an-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    int dp[1005];
+    int help(vector<int>&nums,int k,int idx){
+        if(idx>=nums.size()) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        
+        int ans=INT_MAX;
+        vector<int>freq(nums.size());
+        int trimmed=0;
+        
+        for(int i=idx;i<nums.size();i++){
+            freq[nums[i]]++;
+            if(freq[nums[i]]==2) trimmed+=2;
+            else if(freq[nums[i]]>2) trimmed++;
+            ans=min(ans,help(nums,k,i+1)+k+trimmed);
+        }
+        
+        return dp[idx]=ans;
+    }
+    int minCost(vector<int>& nums, int k) {
+        
+        memset(dp,-1,sizeof(dp));
+        return help(nums,k,0);
+    }
+};
