08b3323a9895a20a53b78976b06f7487d40dd7a6
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-05-18 12:14:12 +0530

    Time: 293 ms (74.99%), Space: 94.2 MB (89.58%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
+ vector<int>parent;
+    int find(int child){
+        if(parent[child]==-1) return child;
+        return parent[child]=find(parent[child]);
+    }
+    
+    void combine(int first,int second){
+        if(find(second)==second)
+        parent[second]=first;
+    }
+    
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<int>in(n);
-        for(auto edge:edges){
-            in[edge[1]]++;
-        }
         vector<int>ans;
+        parent.resize(n,-1);
+       for(auto &it:edges){
+        combine(it[0],it[1]);
+        }
         
-        for(auto i=0;i<n;i++){
-            if(in[i]==0) ans.push_back(i);
+        for(int i=0;i<n;i++){
+            // cout<<i<<" "<<parent[i]<<endl;
+            if(parent[i]==-1) ans.push_back(i);
         }
         return ans;
-    }
+   }
+
 };
