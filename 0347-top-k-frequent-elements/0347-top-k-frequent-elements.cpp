ab0bb01116a222bb7db43ccbf46228d26998b3b8
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-05-22 16:04:32 +0530

    Time: 21 ms (32.68%), Space: 13.9 MB (24.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-   
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int,int>ans;
-        vector<int>res;
-        int x=0;
-        for(int i=0;i<nums.size();i++){
-           ans[nums[i]]++; 
-        }
-         priority_queue<pair<int,int>> pq;
-        // sort(ans.begin(),ans.end(),sortby_sec);
-          for(auto it=ans.begin();it!=ans.end();it++){
-              // cout<<it->first<<" "<<it->second<<"bas ";
-              pq.push(make_pair(it->second, it->first));}
+        unordered_map<int,int>mp;
+        for(auto i:nums) mp[i]++;
         
-             while( k--){
-                 res.push_back(pq.top().second);
-                pq.pop();
-                 
-              }
+        multimap<int,int,greater<int>>freq;
+        for(auto [x,y]:mp) freq.insert(pair<int, int>(y, x));
+        
+        vector<int>ans;
+        
+       for(auto &[x,y]:freq){
+           if(k==0) return ans;
+           else k--,ans.push_back(y);
+       }
+        return ans;
        
-        return res;
-    }
+        }
 };
