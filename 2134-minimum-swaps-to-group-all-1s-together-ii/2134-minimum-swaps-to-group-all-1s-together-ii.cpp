fc23d618797695efd6cd1394bdafe5c26c0aa8d2
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-05-24 04:02:39 +0530

    Time: 124 ms (37.81%), Space: 80.5 MB (68.75%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        int n=nums.size(),count_0;
-        int count_1= count(nums.begin(),nums.end(),1);
- 
-        int windowsize=count_1;
-        count_1=0;
         
-         for(int i=0;i<windowsize;i++){  // always maintain window of size (total 1's)
-             if(nums[i]==0)count_0++;
+        int cnt1=0,cnt0,sz=nums.size();
+        cnt1=count(nums.begin(),nums.end(),1);
+        
+        int windowsize=cnt1;
+        
+        for(int i=0;i<windowsize;i++){
+            if(nums[i]==0) cnt0++;
         }
         
-        int mn=count_0;           //for storing maximum count of 1s in a window
-        for(int i=windowsize;i<n+windowsize;i++){
-            if(nums[i%n]==0)
-                count_0++;
-            if(nums[(i-windowsize)]==0)
-                count_0--;
-            mn=min(mn,count_0);
-        }
+        int mn=cnt0;
         
+        for(int i=windowsize;i<windowsize+sz;i++){
+            if(nums[i%sz]==0) cnt0++;
+            if(nums[i-windowsize]==0) cnt0--;
+            
+            mn=min(mn,cnt0);
+        }
         return mn;
-        
     }
 };
