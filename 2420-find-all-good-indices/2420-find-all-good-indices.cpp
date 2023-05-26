c36f5bf95416c423f319bedbcf105a3246edb680
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-05-27 00:20:26 +0530

    Time: 195 ms (8.16%), Space: 88.9 MB (70.75%) - LeetHub

---
+++ 2420-find-all-good-indices/2420-find-all-good-indices.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> goodIndices(vector<int>& nums, int time) {
+         int sz=nums.size();
+        vector<int>pref(sz,1),suff(sz,1),ans;
+      
+        for(int i=sz-2;i>=0;i--){
+            if(nums[i]<=nums[i+1]) suff[i]=suff[i+1]+1;
+        }
+        for(int i=1;i<sz;i++){
+            if(nums[i]<=nums[i-1]) pref[i]=pref[i-1]+1;
+        }
+        for(int i=time;i<sz-time;i++){
+          if(pref[i-1]>=time and suff[i+1]>=time) ans.push_back(i);
+        }
+        return ans; 
+    }
+};
