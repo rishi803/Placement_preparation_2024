ecd583fcdcdc1d61ea1b431b5dd8eaa722d767f9
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-05-27 00:02:51 +0530

    Time: 176 ms (10.76%), Space: 89.7 MB (57.27%) - LeetHub

---
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> goodDaysToRobBank(vector<int>&nums, int time) {
+        
+        int sz=nums.size();
+        vector<int>pref(sz,0),suff(sz,0),ans;
+      
+        for(int i=sz-2;i>=0;i--){
+            if(nums[i+1]<=nums[i]) pref[i]=pref[i+1]+1;
+        }
+        for(int i=1;i<sz;i++){
+            if(nums[i]>=nums[i-1]) suff[i]=suff[i-1]+1;
+        }
+        for(int i=time;i<sz-time;i++){
+          if(pref[i-time]>=time and suff[i+time]>=time) ans.push_back(i);
+        }
+        return ans;
+    }
+};
