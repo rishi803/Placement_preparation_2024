0cc1938c06d43b4a10ba4338f0a6af2747134b78
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-06-01 15:42:09 +0530

    Time: 331 ms (9.49%), Space: 110.4 MB (23.85%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     
-    void dfs(int x,int y,int row,int col,vector<vector<int>>&land,vector<vector<int>>&temp){
+    void dfs(int x,int y,int row,int col,vector<vector<int>>&land,set<pair<int,int>>&temp){
         if(x<0 or x>=row or y<0 or y>=col or land[x][y]==0) return;
         
         land[x][y]=0;
-        temp.push_back({x,y});
+        temp.insert({x,y});
         
         dfs(x+1,y,row,col,land,temp);
         dfs(x,y+1,row,col,land,temp);
@@ -22,11 +22,11 @@ public:
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(land[i][j]==1){
-                    vector<vector<int>>temp;
+                    set<pair<int,int>>temp;
                     dfs(i,j,row,col,land,temp);
-                    sort(temp.begin(),temp.end());
-                    int tlx=temp[0][0],tly=temp[0][1];
-                    int brx=temp[temp.size()-1][0],bry=temp[temp.size()-1][1];
+                    int tlx=temp.begin()->first,tly=temp.begin()->second;
+                    auto it=--temp.end();
+                    int brx=it->first,bry=it->second;
                     
                     ans.push_back({tlx,tly,brx,bry});
                 }
