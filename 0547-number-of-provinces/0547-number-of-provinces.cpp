5620268b496c27086f0cb64f5090abfcac2a4b5c
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-06-05 01:15:29 +0530

    Time: 20 ms (92.73%), Space: 14.4 MB (51.47%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,51 +1,37 @@
 class Solution {
 public:
     
-    vector<int> parent;
-    
-    //Find the leader of any group in which x lies
-    //if not lie in any group then it is self leader
-    int findparent(int x)
-    {
-        //self leader
-        if(parent[x]==-1) return x; 
-        //find the leader of self parent
-        return parent[x]=findparent(parent[x]);
-    }
-    
-    //Adding 2 friends in a group
-    void _union(int a,int b, int &ans)
-    {
-        //find the leader of both a and b
-        int absroot_a=findparent(a),absroot_b=findparent(b);
-        
-        //if already in same group, i.e leader of both of them are same then return
-        if(absroot_a==absroot_b) return; 
-       ans--;
-        parent[absroot_a]=absroot_b; //v[p_a] will store the index of leader of whole group
-    }
-    
+     void dfs(int i,vector<int>adj[],vector<int>&vis){
+         vis[i]=true;
+         for(auto child:adj[i]){
+             if(!vis[child])
+             dfs(child,adj,vis);
+         }
+     }
     int findCircleNum(vector<vector<int>>& M) { 
        
         int n=M.size();
-         int ans=n;
-         parent.resize(n,-1);  //there will be maximum n group, mark all as a leader
+         int ans=0;
         
+        vector<int>adj[n],vis(n);
         //making group
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(M[i][j]==1)  //if i is friend of j, add them in a group
-                { 
-                    // cout<<i<<" "<<j<<endl;
-                    //if i is in any group then add j in that group
-                    //or vice-versa
-                    _union(i,j,ans);  //Add them in a group
-                    
+                if(M[i][j]==1 and i!=j)  {
+                    adj[i].push_back(j);
                 }
             }
         }
-       return ans;
+        
+       for(int i=0;i<n;i++){
+           if(!vis[i]){
+               // cout<<i<<endl;
+           ans++;
+           dfs(i,adj,vis);    
+           } 
+       }
+        return ans;
     }
 };
