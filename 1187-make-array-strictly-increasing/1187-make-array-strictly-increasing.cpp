3bee8fe8121294c16276e58c32e6ecd016001b51
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-06-17 23:39:40 +0530

    Time: 1070 ms (8.14%), Space: 78 MB (16.86%) - LeetHub

---
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    long long help(vector<int>&arr1,vector<int>&arr2,int idx,int prev, map<pair<int, int>, int>&dp){
+        
+        if(idx==arr1.size()) return 0;
+        
+        if(dp[{idx,prev}] >0 ) {
+            return dp[{idx, prev}];
+        }
+        
+        long long change=INT_MAX,notchange=INT_MAX;
+        
+        if(arr1[idx]>prev){
+            notchange=help(arr1,arr2,idx+1,arr1[idx],dp);
+        }
+        
+    
+            int getidx=upper_bound(arr2.begin(),arr2.end(),prev)-arr2.begin();
+        
+        if(getidx<arr2.size()){
+            change=1+help(arr1,arr2,idx+1,arr2[getidx],dp); 
+        }  
+ 
+        
+        return dp[{idx, prev}]= min(change,notchange);
+    }
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+         map<pair<int, int>, int> dp;
+        sort(arr2.begin(),arr2.end());
+        int ans=help(arr1,arr2,0,-1,dp);
+        return ans==INT_MAX?-1:ans;
+    }
+};
