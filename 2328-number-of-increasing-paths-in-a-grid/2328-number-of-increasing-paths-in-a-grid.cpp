d78917ed4a447c1f043091ccb6524f1bf7473620
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-06-18 21:35:39 +0530

    Time: 260 ms (94.93%), Space: 43.1 MB (96.48%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     
+   ///////////////////////// BRUTE FORCE /////////////////////////////
+    
+//      const int mod=int(1e9+7);
+//      int ans=0;
+    
+//     void help(vector<vector<int>>&grid,int row,int col,int x,int y,int prev){
+        
+//         if(x<0 or x>=row or y<0 or y>=col or grid[x][y]<=prev) return;
+//         int temp=grid[x][y];
+//         ans=(ans+1)%mod;
+        
+//         help(grid,row,col,x+1,y,temp);
+//         help(grid,row,col,x,y+1,temp);
+//         help(grid,row,col,x-1,y,temp);
+//         help(grid,row,col,x,y-1,temp);
+        
+//     }
+    
+//     int countPaths(vector<vector<int>>& grid) {
+        
+//         int row=grid.size(),col=grid[0].size();
+        
+//         for(int i=0;i<row;i++){
+//             for(int j=0;j<col;j++){
+//                 help(grid,row,col,i,j,-1);
+//             }
+//         }
+        
+//         return ans;
+//     }
     
     const int mod=int(1e9+7);
     int dp[1002][1002];
@@ -9,14 +39,17 @@ public:
     long help(vector<vector<int>>&grid,int row,int col,int x,int y,int prev){
         
         if(x<0 or x>=row or y<0 or y>=col or grid[x][y]<=prev) return 0;
-       if(dp[x][y]!=-1) return dp[x][y];
+        
+        if(dp[x][y]!=-1) return dp[x][y];
+        
         int temp=grid[x][y];
+        
         long result=1;
      
-      result+=help(grid,row,col,x+1,y,temp);
-      result+=help(grid,row,col,x,y+1,temp);
-       result+= help(grid,row,col,x-1,y,temp);
-      result+=  help(grid,row,col,x,y-1,temp);
+        result+= help(grid,row,col,x+1,y,temp);
+        result+= help(grid,row,col,x,y+1,temp);
+        result+= help(grid,row,col,x-1,y,temp);
+        result+= help(grid,row,col,x,y-1,temp);
         
         return dp[x][y]=result%mod;
         
@@ -26,6 +59,7 @@ public:
         
         int row=grid.size(),col=grid[0].size();
         memset(dp,-1,sizeof(dp));
+        
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                ans=(ans+help(grid,row,col,i,j,-1))%mod;
