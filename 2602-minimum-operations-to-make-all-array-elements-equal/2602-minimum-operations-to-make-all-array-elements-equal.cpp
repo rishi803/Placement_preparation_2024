69e9ae601f9d82100c2083bd3b85483207432790
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-06-22 00:07:41 +0530

    Time: 296 ms (43.08%), Space: 81.1 MB (53.59%) - LeetHub

---
+++ 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
+        int sz=nums.size();
+       sort(nums.begin(),nums.end());
+       vector<long long>pre(sz);
+        vector<long long>ans;
+        pre[0]=nums[0];
+        for(int i=1;i<sz;i++){
+            pre[i]=pre[i-1]+nums[i];
+        }
+        
+        for(long q:queries){
+            
+            int idx=upper_bound(nums.begin(),nums.end(),q)-nums.begin();
+            if(idx>=nums.size()) ans.push_back (abs(pre[sz-1]-q*(idx)));
+           else if(idx==0) ans.push_back(abs(pre[sz-1]-(q*(sz))));
+            else{
+                long long left=abs(pre[idx-1]-(q*(idx)*1LL));
+                long long right=abs(pre[sz-1]-pre[idx-1]-(q*(sz-idx)*1LL));
+               ans.push_back(left+right);
+            }
+            
+        }
+        return ans;
+    }
+};
