b749b5578254665ff865af70bcbbd693199eb96c
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-06-22 13:55:55 +0530

    Time: 264 ms (28.55%), Space: 95.4 MB (20.38%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     
-    vector<vector<int>>dp;
-    
-    int help(vector<int>&prices,int fee , int idx,bool canbuy){
-        
+    int help(vector<int>&prices,int fee,int idx,bool flag,vector<vector<int>>&dp){
         if(idx>=prices.size()) return 0;
-        if(dp[idx][canbuy]!=-1) return dp[idx][canbuy];
-        int take=0,nottake=0;
+        if(dp[idx][flag]!=-1) return dp[idx][flag];
         
-        if(canbuy){
-            take= -prices[idx]+help(prices,fee,idx+1,false);
-            nottake= help(prices,fee,idx+1,true);
-                
+        int take=INT_MIN,nottake=INT_MIN;
+        
+        if(flag){
+            take= -prices[idx]+help(prices,fee,idx+1,false,dp);
+            nottake=help(prices,fee,idx+1,true,dp);
         }
-        
         else{
-            take= prices[idx]-fee+help(prices,fee,idx+1,true);
-            nottake= help(prices,fee,idx+1,false);
-            
+            nottake=help(prices,fee,idx+1,false,dp);
+            take=prices[idx]-fee+help(prices,fee,idx+1,true,dp);
         }
         
-        return dp[idx][canbuy]=max(take,nottake);
-        
+        return dp[idx][flag]=max(take,nottake);
     }
+    
     int maxProfit(vector<int>& prices, int fee) {
-        dp.resize(prices.size(),vector<int>(2,-1));
-        return help(prices,fee,0,true);
+        
+        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+        return help(prices,fee,0,true,dp);
     }
 };
