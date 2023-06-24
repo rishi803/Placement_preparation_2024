a73c2a30078c3041ce1e35b963ef5d92dbbfa118
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-06-24 22:36:17 +0530

    Time: 714 ms (83.82%), Space: 154.7 MB (66.58%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int ans = 0;
         vector<unordered_map<long long, int>> dp(n); // dp[i][d]
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 long long diff = (long long) nums[i] - nums[j];
                 int cnt = dp[j].count(diff) ? dp[j][diff] : 0;
