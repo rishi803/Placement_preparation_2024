3ceddb4993d15293c8f022d2e9f8a3afc40dcaaf
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-06-29 22:23:03 +0530

    Time: 518 ms (18.82%), Space: 130.5 MB (5.10%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    vector<int>v;
+    int cnt=0;
+    
+    void seive(int n){
+        
+        for(int i=2;i*i<=n;i++){
+            if(v[i]!=1)
+            for(int j=i+i;j<=n;j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    
+    int countPrimes(int n) {
+        // if(n==0) return 0;
+        v.resize(n+1,0);
+        // v[0]=1,v[1]=1;
+        seive(n);
+        
+        for(int i=2;i<n;i++){
+           if(v[i]==0) cnt++;
+        }
+        return cnt;
+    }
+};
