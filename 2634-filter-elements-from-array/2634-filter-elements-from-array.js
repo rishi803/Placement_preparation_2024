a37ffa158694ef6c67b0d62714357cb981ee65ad
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-08 11:15:30 +0530

    Time: 65 ms (19.57%), Space: 41.8 MB (72.64%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,11 @@
+
+var filter = function(arr, fn) {
+   
+    var ans=[];
+    for(var i=0;i<arr.length;i++){
+        if(fn(arr[i],i)){
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
+};
