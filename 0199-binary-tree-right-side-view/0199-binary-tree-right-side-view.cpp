7dda5610c5b6688aacdfabf69f9a080af45d92ac
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-11 02:38:18 +0530

    Time: 3 ms (73.26%), Space: 12 MB (37.38%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -8,15 +8,14 @@ public:
         queue<TreeNode*>q;
         q.push(root);
         int level=0;
-      
+        
         while(!q.empty()){
-              int flag=0;
             int sz=q.size();
             while(sz--){
                 auto node=q.front();
                 q.pop();
                 
-                if(flag==0) ans.push_back(node->val),flag=1;
+                if(ans.size()==level) ans.push_back(node->val);
                 
                 if(node->right) q.push(node->right);
                 if(node->left) q.push(node->left);
