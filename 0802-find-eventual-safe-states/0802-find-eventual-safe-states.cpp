f3723e3c894c0c966e7abfc0bf28a937d6fcc56d
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-13 02:00:23 +0530

    Time: 160 ms (89.06%), Space: 46.5 MB (99.54%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    bool help(vector<vector<int>>&graph,vector<bool>&vis,int node,vector<bool>&pathvis,int idx){
+        
+       
+        vis[node]=true;
+        pathvis[node]=true;
+        
+        for(auto child:graph[node]){
+         if(pathvis[child]==false && help(graph,vis,child,pathvis,idx)){
+                return true;
+            }
+            else if(vis[child]==true){
+            return true;
+        }
+        }
+        // cout<<endl;
+        vis[node]=false;
+        // return true;
+        return false;
+    
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+      
+        vector<bool>vis(graph.size()),pathvis(graph.size());
+        vector<int>ans;
+        
+        for(int i=0;i<graph.size();i++){
+            
+           if(!pathvis[i]){
+             help(graph,vis,i,pathvis,i);
+           }
+               
+            
+        }
+        
+        for(auto i=0;i<vis.size();i++) if(vis[i]==0) ans.push_back(i);
+        return ans;
+    }
+};
