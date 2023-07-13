0258d1d8bfa236fe736e9763443e9a1a3466f37c
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-13 16:20:55 +0530

    Time: 22 ms (77.19%), Space: 13.6 MB (68.39%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
+        
+        vector<int>adj[numCourses];
+        vector<int>indegree(numCourses);
+        queue<int>q;
+        
+        for(auto vec:pre){
+           adj[vec[1]].push_back(vec[0]);
+           indegree[vec[0]]++;
+        }
+        
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
+        while(!q.empty()){
+            int cource=q.front();
+            q.pop();
+            
+            for(auto adjcourse:adj[cource]){
+                indegree[adjcourse]--;
+                if(indegree[adjcourse]==0) q.push(adjcourse);
+            }
+        }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]!=0) return false;
+        }
+        
+        return true;
+    }
+};
