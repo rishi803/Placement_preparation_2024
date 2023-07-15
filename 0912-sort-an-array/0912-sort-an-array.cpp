dde887a64c31824191ec576ed7344b6b9c55b450
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-16 03:48:45 +0530

    Time: 821 ms (28.66%), Space: 180.9 MB (28.33%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,49 @@
 class Solution {
 public:
+    void merge(vector<int>&nums,int st,int mid,int end){
+        int leftsz=mid-st+1;
+        int rightsz=end-mid;
+        
+        vector<int>left(leftsz),right(rightsz);
+        
+        for(int i=0;i<leftsz;i++){
+            left[i]=nums[st+i];
+        }
+        for(int i=0;i<rightsz;i++){
+            right[i]=nums[mid+1+i];
+        }
+        
+        int i=0,j=0,k=st;
+        
+        while(i<leftsz and j<rightsz){
+            if(left[i]<=right[j]){
+                nums[k++]=left[i];
+                i++;
+            }
+            else {
+                nums[k++]=right[j];
+                j++;
+            }
+        }
+        while(i<leftsz){
+            nums[k++]=left[i++];
+        }
+        while(j<rightsz){
+            nums[k++]=right[j++];
+        }
+    }
+    
+    void mergesort(vector<int>&nums,int st,int end){
+        
+        if(st>=end) return;
+        int mid=st+(end-st)/2;
+        mergesort(nums,st,mid);
+        mergesort(nums,mid+1,end);
+        merge(nums,st,mid,end);
+    }
+    
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        mergesort(nums,0,nums.size()-1);
         return nums;
     }
 };
