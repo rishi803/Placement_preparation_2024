09aa50911d3ed0848e37ffecc9977e7a9de47853
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-16 23:07:57 +0530

    Time: 1095 ms (11.11%), Space: 187.1 MB (11.11%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-         map<int,int> mp;
-            int ans = 0;
-            for(int i = 0;i<nums.size();i++){
-                    mp[nums[i] - k] ++;
-                    mp[nums[i] + k + 1] --;
-                    
-            }
-            int prev = 0;
-            for(auto [a,b] : mp){
-                    prev += b;
-                    ans = max(ans,prev);
-            }
-            return ans;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]-k]++;
+            mp[nums[i]+k+1]--;
+        }
+        int mx=0,overlap=0;
+        for(auto [i,j]:mp){
+            overlap=overlap+j;
+            mx=max(mx,overlap);
+        }
+        return mx;
     }
 };
