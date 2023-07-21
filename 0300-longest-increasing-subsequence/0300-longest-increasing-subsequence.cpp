2a4eadc7958d88446e4e4bf45375cb7bfa2e97c8
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-21 10:11:03 +0530

    Time: 585 ms (40.56%), Space: 10.6 MB (51.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    
-    int dp[2502][2502];
-    
-    int help(vector<int>&nums,int idx,int prev){
-     if(idx==nums.size()) return 0;
-     if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-        
-        int take=0,nottake=0;
-        if(prev==-1 or nums[idx]>nums[prev]){
-           take=1+help(nums,idx+1,idx); 
-        }
-         nottake=help(nums,idx+1,prev);
-        
-        return dp[idx][prev+1]=max(take,nottake);
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
+        vector<int>dp(nums.size(),1);
+        int mx=1;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[j]+1,dp[i]);
+                  
+                }
+                  mx=max(mx,dp[i]);
+            }
+        }
         
-        memset(dp,-1,sizeof(dp));
-        return help(nums,0,-1);
+        return mx;
     }
-    
 };
