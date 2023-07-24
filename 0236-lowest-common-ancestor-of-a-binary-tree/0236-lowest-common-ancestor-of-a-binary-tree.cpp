247643b0af3fea41bd8b78842886dc062fd72e8b
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-24 13:26:35 +0530

    Time: 23 ms (26.50%), Space: 14.2 MB (78.81%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,30 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
 class Solution {
 public:
-    // int i=0;
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-     
-      if(!root or root==p or root==q) return root;
-     
-       TreeNode* l= lowestCommonAncestor(root->left,p,q);
-       TreeNode* r= lowestCommonAncestor(root->right,p,q);
-        // i++;
-        //    if(root) cout<<" i= "<<i<<" val= "<<root->val<<endl;
-        // if(!l and r) cout<<"null "<<r->val<<" i= "<<i<<endl;
-        // if(l and !r) cout<<l->val<<" null"<<" i= "<<i<<endl;
-        // if(!l and !r) cout<<"null null"<<" i= "<<i<<endl;
-        // if(l and r)
-        // cout<<l->val<<" "<<r->val<<" i= "<<i<<endl;
-       if(l and r) return root;
-        if(!l) return r;
-         return l;
+        if(!root) return nullptr;
+        if(root==p or root==q) return root;
+        
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        
+        if(left and right) return root;
+        if(!left and right) return right;
+        if(!right and left) return left;
+        else return nullptr;
     }
 };
