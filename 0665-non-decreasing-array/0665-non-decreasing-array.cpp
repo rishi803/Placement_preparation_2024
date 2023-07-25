124fcb34b155aae20f678762a6261aa9ad6402f4
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-25 19:59:23 +0530

    Time: 23 ms (94.58%), Space: 27 MB (69.76%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -5,12 +5,17 @@ public:
         bool flag=false;
         
         for(int i=1;i<nums.size();i++){
-           if(nums[i]<nums[i-1]){
-               if(flag==true) return false;
+           if(nums[i]<nums[i-1]){                  // means some case like--> 5 4
                
-               if(i>1 and nums[i]<nums[i-2]) nums[i]=nums[i-1],flag=true;
-               else if(i==1 or nums[i]>=nums[i-2]) nums[i-1]=nums[i],flag=true;
-               
+               if(flag==true) return false;        // already 1 change has made
+
+               if(i>1 and nums[i]<nums[i-2]) {  // 1st case curr ele < prev ka prev
+                   nums[i]=nums[i-1];          //   1 2 30 50 20    (20 is curr ele) so must increase 20 to 50
+               }
+               else if(i==1 or nums[i]>=nums[i-2]){ // 2nd case curr >= prev ka prev
+                   nums[i-1]=nums[i];             // 1 2 30 50 40  (40 is curr ele) so must down 50 to 40
+               } 
+               flag=true;                 // we have made a change
            }
         }
         return true;
