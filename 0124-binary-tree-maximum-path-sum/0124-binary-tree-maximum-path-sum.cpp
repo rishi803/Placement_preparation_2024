d6e03a6108f3e417cbe4f112f99cbc339aece1db
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-26 19:29:38 +0530

    Time: 40 ms (9.29%), Space: 28.5 MB (8.95%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -4,23 +4,26 @@ public:
     int ans=-1e4;
     
     int help(TreeNode* root){
+        
         if(!root) return -1e4;
         
         int leftsum= help(root->left);
         int rightsum=help(root->right);
         
        ans=max({
-           ans,
-           root->val,
-           leftsum,      
-           rightsum,
-           root->val+leftsum,
-           root->val+rightsum,
-           root->val+leftsum+rightsum
+           ans,                // update answer
+           root->val,          // this root can be max
+           leftsum,            // max answer lies in right
+           rightsum,           // max answer lies in left 
+           root->val+leftsum,  // max answer can be (root+left)
+           root->val+rightsum, // max answer can be (root+right)
+           root->val+leftsum+rightsum // max answer can be (root+left+right);
        });
-        
+        // cout<<leftsum<<" "<<rightsum<<" "<<root->val<<endl;
         return max({
-            root->val,
+            root->val,         
+            // leftsum,          // [1,-2,-3,1,3,-2,null,-1] failing 
+            // rightsum,         // [1,-2,-3,1,3,-2,null,-1] failing
             root->val+leftsum,
             root->val+rightsum
         });
