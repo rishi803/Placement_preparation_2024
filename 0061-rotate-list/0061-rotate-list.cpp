e518397c99dd1919139153732f0489bf96a910c5
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-28 02:08:55 +0530

    Time: 5 ms (80.17%), Space: 11.7 MB (58.86%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -6,26 +6,27 @@ public:
         ListNode* temp=head;
         int len=1;
         
-        while(temp and temp->next){
-            temp=temp->next;
+        while(temp and temp->next){      // find complete ll length
+            temp=temp->next;             // send this temp to last node
             len++;
         }
      
-        ListNode* prev=head;
-        k=k%len;
+        ListNode* prev=head;      
+        
+        k=k%len;     // rotation can only done upto its length (len=5, rotation_possible=1,2,3,4)
         
         if(k==0) return head;
         
         else
         {
-            k=len-k-1;
-            while(k--){
+            k=len-k-1;          // where "k" rotate start from start
+            while(k--){         // send this "prev" just before to there "k" rotate start from node
                 prev=prev->next;
             }
 
-            temp->next=head;
-            head=prev->next;
-            prev->next=nullptr;
+            temp->next=head;     // now point last node to head of the ll
+            head=prev->next;     // now head will be next node of prev where rotation starts
+            prev->next=nullptr;  // prev ka next will now become last node pointing null value
             
         }
         return head;
