5ede285d3ca7e76fc942bca01c459308e463ca70
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-31 01:07:08 +0530

    Time: 4 ms (98.40%), Space: 7.7 MB (93.29%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        
-        int head=0,tail=0,minstring_start=0,minlen=INT_MAX;
-        int sz_required=0;
-        
+        int head=0,tail=0,minlen=INT_MAX,start_pos=0,sz=0;
         int map[128]={};
-        for(auto i:t) map[i]++;
+        
+        for(auto it:t){
+            map[it]++;
+        }
         
         for(head=0;head<s.size();head++){
+            map[s[head]]--;           // operation
             
-            if(map[s[head]]>0) sz_required++;
-            map[s[head]]--;
-        
-            while(sz_required==t.size()){
-                
+            if(map[s[head]]>=0) sz++;
+            
+            while(sz==t.size()){
                 if(minlen > (head-tail+1)){
-                    minlen=head-tail+1;
-                    minstring_start=tail;
+                    minlen= head-tail+1;
+                    start_pos= tail;
                 }
-                  map[s[tail]]++;
-            
-              if(map[s[tail]]>0) sz_required--;
-                tail++;
+                map[s[tail]]++;
                 
-                } 
+                if(map[s[tail]]>0) sz--;
+                tail++;
+            }
         }
-      
-        return minlen==INT_MAX?"":s.substr(minstring_start,minlen);
+        
+        return minlen==INT_MAX?"": s.substr(start_pos,minlen);
     }
 };
