f50636e7291f6223333225ae62c17a27b09c5baa
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-01 01:20:03 +0530

    Time: 159 ms (96.25%), Space: 113 MB (88.58%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -20,9 +20,19 @@ class Solution {
             int sum= root->val + left.sum + right.sum;
             ans=max(ans,sum);
             
-            return {sum,max(root->val,max(left.mx,right.mx)),min(root->val,min(right.mn,left.mn)),true};
+            return {
+                    sum,
+                    max(root->val,max(left.mx,right.mx)),     // jo sbase bdi value h subtree me
+                    min(root->val,min(right.mn,left.mn)),     // jo sabse choti value h subtree me
+                    true        // han bhai bst to hoga hi phir agr is loop me hai
+                    };
         }
-        else return {0,INT_MAX,INT_MIN,false};
+        else return {
+            0,         // kuch nhi mila \U0001f62b
+            123,       // kuch bhi likho ab yaha iska koi contribution nhi ab
+            98,        // no use iska bhi kunki ab kabhi upar bst milega hi nhi
+            false      // bst nhi hai phir aur kya \U0001f972
+            };
         
     }
     
