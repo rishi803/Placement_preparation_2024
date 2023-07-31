ad1dbf7d3ee6289f8f0ea5199f12878aa99bc9f6
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-01 00:31:48 +0530

    Time: 23 ms (90.69%), Space: 7.6 MB (84.58%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
+    
     int dp[502][502];
-    int help(string& word1,int n,string& word2,int m){
-        if(n==0){
-            return accumulate(word2.begin(),word2.begin()+m,0);
-        }
-        if(m==0) {
-            return accumulate(word1.begin(),word1.begin()+n,0);
-        }
-        if(dp[m][n]!=-1) return dp[m][n];
-        int op1=INT_MAX,op2=INT_MAX;
+    
+    int help(string &s1,string &s2,int i,int j){
+        if(i==s1.size() and j==s2.size()) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i==s1.size())  return accumulate(s2.begin()+j,s2.end(),0);
+        if(j==s2.size()) return accumulate(s1.begin()+i,s1.end(),0);
         
-        if(word1[n-1]==word2[m-1]){
-            op1= help(word1,n-1,word2,m-1);
-        }
+        if(s1[i]==s2[j]) return dp[i][j]= help(s1,s2,i+1,j+1);
         else{
-            op2= min(word1[n-1]+help(word1,n-1,word2,m),word2[m-1]+help(word1,n,word2,m-1));
+            return dp[i][j]= min(s1[i]+help(s1,s2,i+1,j),s2[j]+help(s1,s2,i,j+1));
         }
-        
-        return dp[m][n]=min(op1,op2);
     }
     
     int minimumDeleteSum(string s1, string s2) {
-          memset(dp,-1,sizeof(dp));
-        int n=s1.size();
-        int m=s2.size();
-        // vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return help(s1,n,s2,m);
+        memset(dp,-1,sizeof(dp));
+        return help(s1,s2,0,0);
     }
 };
