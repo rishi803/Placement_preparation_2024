7132814c426e38059574207b8adc3d5affdbe5bd
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-01 19:59:29 +0530

    Time: 1690 ms (55.28%), Space: 6.8 MB (42.22%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+   
+    
+    
+    bool ispalin(string &s , int st, int end){
+       while(st<=end){
+           if(s[st++]!=s[end--]) return false;
+       }
+        return true;
+    }
+    
+    int help(string &s, int idx,vector<int>&dp){
+        if(idx==s.size()) return -1;
+        if(dp[idx]!=-1) return dp[idx];
+        
+        int ans=INT_MAX;
+        
+        for(int i=idx;i<s.size();i++){
+            if(ispalin(s,idx,i)){
+                ans=min(ans,1+help(s,i+1,dp));
+            }
+        }
+       return dp[idx]= ans;
+        
+    }
+    
+    int minCut(string s) {
+        vector<int>dp(2001,-1);
+        return help(s,0,dp);
+    }
+};
