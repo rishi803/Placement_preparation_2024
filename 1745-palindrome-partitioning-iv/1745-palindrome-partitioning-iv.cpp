1a32ab35277b7f634168b05bbc3986ccdaf95916
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-01 22:00:20 +0530

    Time: 2367 ms (15.51%), Space: 10.3 MB (87.65%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     
     int sz;
+    vector<vector<int>>dp;
     
     bool ispalin(string &s, int st, int end){
         while(st<=end){
@@ -11,23 +12,25 @@ public:
     }
     
     bool help(string &s,int idx,int partition){
-        if(idx>=s.size()) return partition==0;
         
+        if(idx>=s.size()) return partition==0;
+        if(dp[idx][partition]!=-1) return dp[idx][partition];
         if(partition==0) return ispalin(s,idx,sz-1);
         
         bool ans=false;
         
-        for(int i=idx;i<sz-1;i++){
+        for(int i=idx;i<sz-1;i++){   // why sz-1 and partition=2 because it make sure that partition become 0 it means we have make 2 cuts surely and there must be some remaining string which is palindrome
             if(ispalin(s,idx,i)){
                 ans|= help(s,i+1,partition-1);
             }
         }
         
-        return ans;
+        return dp[idx][partition]=ans;
     }
     
     bool checkPartitioning(string s) {
         
+        dp.resize(s.size(),vector<int>(3,-1));
         sz=s.size();
         return help(s,0,2);
     }
