219460345fc7b4d07ba43f6bc616a7b0b42c6a87
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-02 18:54:34 +0530

    Time: 3 ms (67.29%), Space: 7.9 MB (39.26%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
+    
     vector<vector<int>>ans;
     
-    void help(int st,int end,vector<int>nums){
-        
-        // base condition to stop recursion
-        
-        if(st==end){
+    void help(vector<int>&nums,int idx){
+        if(idx==nums.size()){
             ans.push_back(nums);
             return;
         }
         
-        for(int i=st;i<=end;i++){
-            swap(nums[st],nums[i]);
-            st++;
-            help(st,end,nums);
-            st--;  // backtrack
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[i],nums[idx]);
+            help(nums,idx+1);
+            swap(nums[i],nums[idx]);
         }
     }
+    
     vector<vector<int>> permute(vector<int>& nums) {
-        int st=0,end=nums.size()-1;
-        help(st,end,nums);
+        vector<int>temp;
+        help(nums,0);
         return ans;
     }
 };
