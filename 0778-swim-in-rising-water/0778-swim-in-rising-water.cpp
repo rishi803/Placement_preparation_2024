426cbd63b71350ebcc391b4cc32e451556a87ff2
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-03 22:33:30 +0530

    Time: 1028 ms (5.01%), Space: 43.7 MB (5.01%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int dp[51][51][3000];
-    long long rec(vector<vector<int>>&grid,int i,int j,int time, int row,int col,vector<vector<int>>&vis){
+    long long rec(vector<vector<int>>&grid,int i,int j,int time, int row,int col){
         
-      if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || vis[i][j])
+      if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() or grid[i][j]==-1)
             return INT_MAX;
         
         if(i==grid.size()-1 && j==grid[0].size()-1)
@@ -12,16 +12,19 @@ public:
         if(dp[i][j][time]!=-1)
             return dp[i][j][time];
         
-        vis[i][j]=1;
+        int temp=grid[i][j];
+        time= max(time,grid[i][j]);
+        
+        grid[i][j]=-1;
         
         long long op1,op2,op3,op4;
         
-        op1=rec(grid,i,j+1,max(time,grid[i][j]),row,col,vis);
-        op2=rec(grid,i+1,j,max(time,grid[i][j]),row,col,vis);
-        op3=rec(grid,i,j-1,max(time,grid[i][j]),row,col,vis);
-        op4=rec(grid,i-1,j,max(time,grid[i][j]),row,col,vis);
+        op1=rec(grid,i,j+1,time,row,col);
+        op2=rec(grid,i+1,j,time,row,col);
+        op3=rec(grid,i,j-1,time,row,col);
+        op4=rec(grid,i-1,j,time,row,col);
         
-        vis[i][j]=0;
+        grid[i][j]= temp;
         
         return dp[i][j][time]=min({op1,op2,op3,op4});
         
@@ -32,7 +35,7 @@ public:
         
         int row=grid.size();
         int col=grid[0].size();
-         vector<vector<int>>vis(row,vector<int>(col,0));
-        return rec(grid,0,0,0,row,col,vis);
+    
+        return rec(grid,0,0,0,row,col);
     }
 };
