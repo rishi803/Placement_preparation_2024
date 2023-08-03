d1489c89997a10512325720963968593e25b919f
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-03 23:17:36 +0530

    Time: 8 ms (98.88%), Space: 8.7 MB (94.73%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,41 +1,52 @@
 class Solution {
 public:
-    int dp[51][51][3000];
-    long long rec(vector<vector<int>>&grid,int i,int j,int time, int row,int col){
+    int dp[51][51];
+    
+    bool rec(vector<vector<int>>&grid,int i,int j,int mid, int row,int col){
         
-      if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() or grid[i][j]==-1)
-            return INT_MAX;
+      if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() or grid[i][j]<0 or grid[i][j]>mid)
+            return false;
         
         if(i==grid.size()-1 && j==grid[0].size()-1)
-            return max(time,grid[i][j]);
+            return true;
         
-        if(dp[i][j][time]!=-1)
-            return dp[i][j][time];
+        if(dp[i][j]!=-1)
+            return dp[i][j];
         
-        int temp=grid[i][j];
-        time= max(time,grid[i][j]);
+        grid[i][j]*=-1;
         
-        grid[i][j]=-1;
+       
+     dp[i][j]=  rec(grid,i,j+1,mid,row,col)
+             or rec(grid,i+1,j,mid,row,col) 
+             or rec(grid,i,j-1,mid,row,col) 
+             or rec(grid,i-1,j,mid,row,col);
         
-        long long op1,op2,op3,op4;
-        
-        op1=rec(grid,i,j+1,time,row,col);
-        op2=rec(grid,i+1,j,time,row,col);
-        op3=rec(grid,i,j-1,time,row,col);
-        op4=rec(grid,i-1,j,time,row,col);
-        
-        grid[i][j]= temp;
-        
-        return dp[i][j][time]=min({op1,op2,op3,op4});
+        grid[i][j]*=-1;
+         
+        return dp[i][j];
         
     }
     int swimInWater(vector<vector<int>>& grid) {
-        
-       memset(dp,-1,sizeof(dp));
-        
+    
         int row=grid.size();
         int col=grid[0].size();
     
-        return rec(grid,0,0,0,row,col);
+       int low=0,high=row*row;
+        int ans=-1;
+        
+        while(low<=high){
+            
+            memset(dp,-1,sizeof(dp));
+            int mid= low+(high-low)/2;
+            
+            if(rec(grid,0,0,mid,row,col)){
+                ans=mid;
+                high= mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 };
