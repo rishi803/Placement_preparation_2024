031d331098925565e8b0b077c36649fed45558e2
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-05 00:37:32 +0530

    Time: 18 ms (99.71%), Space: 19.6 MB (83.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        if(strs.empty()) return {{}};
-        vector<pair<string,string>>ve;
-        vector<vector<string>>res;
-        vector<string>temp;
-        for(int i=0;i<strs.size();i++){
-          string tem=strs[i];
-            sort(tem.begin(),tem.end());
-             ve.push_back({tem,strs[i]});
+        unordered_map<string,vector<string>>mp;
+        vector<vector<string>>ans;
+        
+        for(int i=0; i<strs.size(); i++){
+            
+            string temp= strs[i];
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back( strs[i] );
+            
         }
-        sort(ve.begin(),ve.end());
-        // for(auto it:ve){
-        //     cout<<it.first<<" "<<it.second<<endl;
-        // }
-        for(int i=0;i<strs.size();i++){
-          
-            temp.emplace_back(ve[i].second);
-            if( i<strs.size()-1 and ve[i].first!=ve[i+1].first){
-                res.emplace_back(temp);
-                temp.clear();
-            }
+        
+        for(auto &[x,y] : mp){
+            ans.push_back(y);
         }
-        res.emplace_back(temp);
-        return res;
+        
+        return ans;
     }
 };
