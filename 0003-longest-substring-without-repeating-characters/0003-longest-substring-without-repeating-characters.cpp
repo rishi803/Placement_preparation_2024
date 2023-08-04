060568772f74e9adfac6e18896856e13767e1de2
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-04 22:16:48 +0530

    Time: 14 ms (75.34%), Space: 8.4 MB (55.89%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-  int lengthOfLongestSubstring(string s) {
-        vector<int> dict(256, -1);
-        int maxLen = 0, start = -1;
-        for (int i = 0; i != s.length(); i++) {
-            if (dict[s[i]] > start)
-                start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int>mp;
+        int head=0,tail=0,mxlen=0;
+        
+        for(head=0;head<s.size();head++){
+            mp[s[head]]++;
+            
+            while(mp[s[head]] > 1){
+                mp[s[tail]]--;
+                tail++;
+            }
+            
+            mxlen=max(mxlen,head-tail+1);
         }
-        return maxLen;
+        
+        return mxlen;
     }
 };
