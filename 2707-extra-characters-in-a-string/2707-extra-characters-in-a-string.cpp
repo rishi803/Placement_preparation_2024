c90eed08bd7c4707cabfbff39b3017bc990e747a
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-05 02:48:27 +0530

    Time: 421 ms (25.56%), Space: 224.6 MB (8.93%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    int minExtraChar(string s, vector<string>& dictionary) {
+    bool wordBreak(string s, vector<string>& wordDict) {
+      int sz=s.size();
         
-        unordered_map<string,int>mp;
-        for(auto str: dictionary){
-            mp[str] = 1;
+      vector<int>dp(sz+1);
+      dp[0]=0;
+        
+        for(int i=1;i<=sz;i++){
+            dp[i]=1+dp[i-1];
+            for(int j=i;j>0;j--){
+    
+            string temp=s.substr(j-1,i-j+1);
+                // cout<<temp<<endl;
+                
+            if(find(wordDict.begin(),wordDict.end(),temp)!=wordDict.end()){
+                dp[i]=min(dp[i],dp[j-1]);
+            }
+                
+            }
+            // cout<<endl;
         }
         
-        vector<int>dp(s.size()+1);
-        dp[0]=1;
+        // for(int i=0;i<=sz;i++){
+        //     cout<<dp[i]<<" ";
+        // }
         
-        for(int i=0; i<s.size(); i++){
-            if(i!=0) dp[i] = 1 + dp[i-1];
-            
-         for(int j=i; j>=0; j--){
-             
-             string str= s.substr(j, i-j+1);
-             if(mp[str] > 0){
-                 
-                 if(j==0) dp[i]=0;
-                 else
-                 dp[i]= min(dp[i] , dp[j-1]);
-             }
-         }
-            
-       }
-        
-        return dp[s.size()-1];
+        return dp[sz]==0?true:false;
     }
 };
