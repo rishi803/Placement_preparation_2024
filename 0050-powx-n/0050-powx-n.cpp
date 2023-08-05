fdcefe00935df3053641cb15a3ea64b8eb62ad47
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-05 14:31:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
+    
+    double help( double num, long pow){
+        
+        if(pow == 0) return 1;
+        
+        double calculate_pow= help(num, pow/2);
+        
+        if( pow & 1){
+            return num * calculate_pow * calculate_pow;
+        }
+        
+        else{
+            return calculate_pow * calculate_pow;
+        }
+    }
+    
     double myPow(double x, int n) {
-         return pow(x,n);
+        
+        long long m= n;
+        
+        if(n < 0){
+            x = 1/x;
+            m= -m;
+        }
+        return help(x,m);
     }
 };
