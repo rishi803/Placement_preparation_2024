ded56eae30b5d575e00bc9ef7cb7b4565d355c76
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-06 01:50:58 +0530

    Time: 13 ms (99.10%), Space: 22.3 MB (28.79%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,3 +1,26 @@
+// index:   0 1 2 3 4 5 6 7 8
+// vector: [1,1,2,3,3,4,4,8,8]
+//                   \U0001f446 (Target element is 2 since it appears once)
+// In this example, before 2 every first instance of element occurs at even index and second instance at odd index (see element 1) and after 2 every first instance of element occurs at odd index and second instance at even index (see element 3, 4 and 8).
+
+// So from the above observation we will apply binary search on our array, and if we are on the PART - B side of array, we go to left, otherwise we go right.
+
+// mid^1 explanation:
+// If mid is odd then (mid^1) will always be the even number before mid
+// If mid is even then (mid^1) will always be the odd number after mid
+
+// And hence I am able to check whether mid is in left half or right half as:
+
+// Suppose mid is odd: then mid^1 will give even number before it, 
+// then if nums[mid] == nums[mid^1] 
+// then 1st instance at even (mid^1) and 2nd instance at odd (mid), 
+// hence we are on left side otherwise we are on right side.
+
+// Suppose mid is even: then mid^1 will give odd number after it, 
+// then if nums[mid] == nums[mid^1]
+// then 1st instance at even (mid) and 2nd instance at odd (mid^1), 
+// hence we are on left side otherwise we are on right side
+
 class Solution {
 public:
    // Binary Search Approach
@@ -9,7 +32,7 @@ int singleNonDuplicate(vector<int>& nums) {
     while(start<=end and nums.size()>1){     
         int mid = start + (end - start)/2;
         
-        if(nums[mid] == nums[mid^1])
+        if(mid&1 and nums[mid] == nums[mid-1] or mid%2==0 and nums[mid] == nums[mid+1])
             start = mid + 1;
         else
             end = mid - 1;
