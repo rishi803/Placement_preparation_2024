7ce9592fc797e6cad4502941d47384d524057b41
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-08 01:16:00 +0530

    Time: 21 ms (85.30%), Space: 26.9 MB (95.25%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -2,22 +2,32 @@ class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
         
-        bool flag=false;
+        bool modification= false;
         
-        for(int i=1;i<nums.size();i++){
-           if(nums[i]<nums[i-1]){                  // means some case like--> 5 4
-               
-               if(flag==true) return false;        // already 1 change has made
-
-               if(i>1 and nums[i]<nums[i-2]) {  // 1st case curr ele < prev ka prev
-                   nums[i]=nums[i-1];          //   1 2 30 50 20    (20 is curr ele) so must increase 20 to 50
-               }
-               else if(i==1 or nums[i]>=nums[i-2]){ // 2nd case curr >= prev ka prev
-                   nums[i-1]=nums[i];             // 1 2 30 50 40  (40 is curr ele) so must down 50 to 40
-               } 
-               flag=true;                 // we have made a change
-           }
+        for(int i=0; i<nums.size(); i++){
+            
+            if(i>=1){
+                 if(nums[i] < nums[i-1]){
+                     
+                     if(modification) return false;
+                     
+                     if(i-2 >= 0 and nums[i] >= nums[i-2]){
+                         nums[i-1]= nums[i];
+                         modification=true;
+                     }
+                     else if(i-2>=0){
+                         nums[i]=nums[i-1];
+                         modification=true;
+                     }
+                     else{
+                         nums[i-1]= nums[i];
+                         modification= true;
+                     }
+            }
+            }
+           
         }
+        
         return true;
     }
 };
