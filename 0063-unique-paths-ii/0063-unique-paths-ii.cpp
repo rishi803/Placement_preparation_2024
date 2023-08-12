bcbcab9d76c153f139b15061d30c9c64c487c830
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-12 19:21:33 +0530

    Time: 2 ms (68.17%), Space: 7.6 MB (90.03%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,18 +5,18 @@ public:
     
     int help(vector<vector<int>>&grid, int x, int y, int row, int col){
         
-        if(x<0 or y<0 or x>=row or y>=col or grid[x][y]==1 or grid[x][y]==-1) return 0;
+        if(x<0 or y<0 or x>=row or y>=col or grid[x][y]==1) return 0;
         
         if(x== row-1 and y==col-1) return 1;
         
         if(dp[x][y] != -1) return dp[x][y];
         
-             grid[x][y]*=-1;
+             // grid[x][y]*=-1;
         
              int right= help(grid, x, y+1, row, col);
              int down=  help(grid, x+1, y, row, col);
         
-            grid[x][y]*=-1;
+            // grid[x][y]*=-1;
         
         return dp[x][y]= (right + down);
         
