1a2fb2e816b230efcfb5d2320fd6b50ce5a02609
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-14 18:35:03 +0530

    Time: 157 ms (91.90%), Space: 24.3 MB (46.01%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -1,37 +1,42 @@
+// Exactly similar code as https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/?envType=list&envId=oc88tln3
+
 class Solution {
 public:
     
-    int sz;
-    vector<vector<int>>dp;
+    int dp[2001][4];
     
-    bool ispalin(string &s, int st, int end){
-        while(st<=end){
-            if(s[st++]!=s[end--]) return false;
-        }
-        return true;
+   int paldp[2001][2001];
+    bool ispal(string &s, int i, int j)
+    {
+        if(i>=j) return paldp[i][j] = true;
+        if(paldp[i][j]!=-1) return paldp[i][j];
+        if(s[i]!=s[j]) return paldp[i][j] = false;
+        return paldp[i][j] = ispal(s, i+1, j-1);
     }
     
-    bool help(string &s,int idx,int partition){
+    int help(string &s, int idx, int cut){
         
-        if(idx>=s.size()) return partition==0;
-        if(dp[idx][partition]!=-1) return dp[idx][partition];
-        if(partition==0) return ispalin(s,idx,sz-1);
+        if(cut == 0 and idx >= s.size()) return true;
+        if(cut < 0 or idx >= s.size()) return false;
+        if( dp[idx][cut] != -1) return dp[idx][cut];
         
-        bool ans=false;
+        bool ans= false;
         
-        for(int i=idx;i<sz-1;i++){   // why sz-1 and partition=2 because it make sure that partition become 0 it means we have make 2 cuts surely and there must be some remaining string which is palindrome
-            if(ispalin(s,idx,i)){
-                ans|= help(s,i+1,partition-1);
-            }
+        for(int i= idx; i<s.size(); i++){
+            
+             if( ispal (s, idx, i) ){
+                 ans= ans | (help(s, i+1, cut-1));
+             }
+            
         }
         
-        return dp[idx][partition]=ans;
+        return dp[idx][cut]= ans;
+        
     }
     
     bool checkPartitioning(string s) {
-        
-        dp.resize(s.size(),vector<int>(3,-1));
-        sz=s.size();
-        return help(s,0,2);
+          memset(paldp, -1, sizeof(paldp));
+          memset(dp, -1, sizeof(dp));
+          return help(s, 0, 3);
     }
 };
