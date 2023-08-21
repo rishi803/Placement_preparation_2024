760091afd56581f992c1b36197d47433a51d3452
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-21 21:59:35 +0530

    Time: 18 ms (67.67%), Space: 18.4 MB (34.73%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -5,7 +5,7 @@ public:
         bool ans=false;
         int j=0;
         
-        for(int i=0; i<s.size(); i++){
+        for(int i=0; i<s.size()/2; i++){
             
             if(s.size() % (i+1)==0){
                 
