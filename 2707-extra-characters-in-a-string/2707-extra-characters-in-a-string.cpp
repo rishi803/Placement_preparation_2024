77e501ce2dbe4ded7198e4da5d3fada362f452a4
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-22 01:18:13 +0530

    Time: 398 ms (23.28%), Space: 224.5 MB (9.96%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-      int sz=s.size();
+    int minExtraChar(string s, vector<string>& dictionary) {
         
-      vector<int>dp(sz+1);
-      dp[0]=0;
+        int sz= s.size();
         
-        for(int i=1;i<=sz;i++){
-            dp[i]=1+dp[i-1];
-            for(int j=i;j>0;j--){
-    
-            string temp=s.substr(j-1,i-j+1);
-                // cout<<temp<<endl;
+        unordered_map<string,int>hash;
+        
+        for(auto i:dictionary) hash[i]=1;
+        
+        vector<int>dp(sz);
+        dp[0]= 1;
+        
+        for(int current= 0; current<sz; current++){
+            
+            if(current != 0) dp[current]= dp[current-1] + 1;
+            
+            for(int start= current; start>=0; start--){
                 
-            if(find(wordDict.begin(),wordDict.end(),temp)!=wordDict.end()){
-                dp[i]=min(dp[i],dp[j-1]);
-            }
+                string substr= s.substr(start, current-start+1);
                 
+                if(hash[substr] > 0){
+                    
+                    if(start == 0){
+                        dp[current]= 0;
+                    }
+                    else{
+                        
+                        dp[current]= min(dp[current],dp[start-1]);
+                        
+                    }
+                }
+               
+                // cout<<start<<" "<<substr<<dp[current]<<endl;
             }
-            // cout<<endl;
+            // cout<<dp[current]<<endl;
         }
-        
-        // for(int i=0;i<=sz;i++){
-        //     cout<<dp[i]<<" ";
-        // }
-        
-        return dp[sz]==0?true:false;
+        // for(auto i=0; i<dp.size(); i++) cout<<i<<" "<<dp[i]<<endl;
+        return dp[sz-1];
     }
 };
