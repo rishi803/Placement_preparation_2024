e1687c9462e9d61b08359756396046c9ac5f84f9
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-23 00:03:29 +0530

    Time: 2 ms (49.78%), Space: 6 MB (37.93%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        
+        string ans;
+        
+       while(columnNumber > 0){
+          --columnNumber;
+           ans+=('A'  +  columnNumber % 26);
+           columnNumber/=26;
+       }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
