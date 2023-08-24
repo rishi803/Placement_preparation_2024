2e1b9e92a3871aa75e20f7055de3bf0e5c2013ec
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-24 14:48:35 +0530

    Time: 7 ms (56.52%), Space: 13.7 MB (63.77%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,18 +1,20 @@
-// GOAL-->   upto  current element find max and min product and also check whther max product and min product start from current or continues from previous
-
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int ans=nums[0],mx=1,mn=1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<0) swap(mn,mx);
-            mn=min(nums[i],nums[i]*mn); // at current pos check whther we get min from previous element(means subarrays continues or subarray  will start from this current element)
+        
+        int mx=1, mn=1, ans= INT_MIN;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] < 0){
+                swap(mx, mn);
+            }
             
-            mx=max(nums[i],nums[i]*mx);
-            // cout<<"mx"<<" "<<mx<<endl;
-            ans=max(mx,ans);
-            // cout<<i<<"ans"<<" "<<ans<<endl;
+            mx= max(nums[i], mx*nums[i]);
+            mn= min(nums[i], mn*nums[i]);
+            
+            ans= max(ans, mx);
         }
+        
         return ans;
     }
 };
