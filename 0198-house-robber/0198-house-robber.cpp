a656d97fa9f540013810a22af801744078042aef
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-08-26 21:10:56 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (18.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-   
-    vector<int>memo;
-    
-    
-   int help(vector<int>& nums, int idx) {
-           if (idx >= nums.size()) {
-            return 0;
-        }
-
-        if (memo[idx] != -1) {
-            return memo[idx];
-        }
-
-        int money = 0;
-
-        for (int i = idx; i < nums.size(); ++i) {
-            int current = nums[i] + help(nums, i + 2);
-            money = std::max(money, current);
-        }
-
-        memo[idx] = money;
-        return money;
-    }
-    
     int rob(vector<int>& nums) {
-        memo.resize(nums.size(),-1);
-      return  help(nums,0);
-      
+        
+        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 2) return max(nums[0], nums[1]);
+        
+        int dp[101];
+        memset(dp, -1, sizeof(dp));
+        dp[0]= nums[0];
+        dp[1]= max(nums[0],nums[1]);
+        
+        for(int i=2; i<nums.size(); i++){
+            dp[i]= max(nums[i]+dp[i-2], dp[i-1]);
+        }
+        
+        return dp[nums.size()-1];
     }
 };
