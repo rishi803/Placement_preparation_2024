6cdb33388113c43c8c7071f28b5e6e0ca6559804
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-09-01 12:23:47 +0530

    Time: 6 ms (56.68%), Space: 7.8 MB (89.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,21 +2,28 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         
-        vector<int>ans;
+        if(n == 0) return {0};
+        if(n == 1) return {0,1};
+        if(n == 2) return {0,1,1};
         
-        for(int i=0; i<=n; i++){
+        vector<int>ans(n+1);
+        ans[0]= 0;
+        ans[1]= 1;
+        ans[2]= 1;
+        
+        int pow2= 2;
+        
+        for(int i=3; i<=n; i++){
             
-            int cnt1= 0;
-            int temp= i;
             
-            while(temp > 0){
-                int dig= temp & 1;
-                if(dig == 1) cnt1++;
-                
-                temp>>=1;
+            if(i % pow2 == 0){
+                pow2= i, ans[i]= 1;
+            } 
+            
+            else{
+               int idx= i % pow2;
+               ans[i]= 1+ans[idx];
             }
-           // cout<<i<<endl;
-            ans.push_back(cnt1);
         }
         
         return ans;
