6b91bde271d26b5366dfe7c8c726deb793ff845f
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-09-05 21:34:01 +0530

    Time: 2339 ms (12.50%), Space: 6.6 MB (88.99%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -1,45 +1,34 @@
-// Exactly similar code as https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/?envType=list&envId=oc88tln3
-
 class Solution {
 public:
     
-    int dp[2001][4];
-   int paldp[2001][2001];
     
-    
-    bool ispal(string &s, int i, int j)
-    {
-        if(i>=j) return paldp[i][j] = true;
-        if(paldp[i][j]!=-1) return paldp[i][j];
-        if(s[i]!=s[j]) return paldp[i][j] = false;
-        return paldp[i][j] = ispal(s, i+1, j-1);
+    bool ispalin(string &s, int st, int end){
+        
+        while(st <= end){
+            if(s[st++] != s[end--]) return false;
+        }
+        
+        return true;
     }
     
     int help(string &s, int idx, int cut){
+        if(idx >= s.size()) return 0;
+        if(cut==0) return ispalin(s, idx, s.size()-1);
         
-        if(cut == 0 and idx >= s.size()) return true;
-        if(cut < 0 or idx >= s.size()) return false;
-        if( dp[idx][cut] != -1) return dp[idx][cut];
-        
-        // bool ans= false;
+        bool ans= false;
         
         for(int i= idx; i<s.size(); i++){
             
-             if( ispal (s, idx, i) ){
-                 // ans= ans | (help(s, i+1, cut-1));
-                 if(help(s, i+1, cut-1)) return dp[idx][cut]= true;
-             }
-            
+            if(ispalin(s, idx, i)){
+                ans= ans or help(s, i+1, cut-1);
+            }
         }
         
-        // return dp[idx][cut]= ans;
-           return dp[idx][cut]= false;
-        
+        return ans;
     }
     
     bool checkPartitioning(string s) {
-          memset(paldp, -1, sizeof(paldp));
-          memset(dp, -1, sizeof(dp));
-          return help(s, 0, 3);
+        
+        return help(s, 0, 2);
     }
 };
