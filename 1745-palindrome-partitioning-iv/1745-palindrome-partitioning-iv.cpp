cf8a799306cf074a2ca2bb31beb7726b95918b36
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-09-05 21:42:19 +0530

    Time: 148 ms (90.86%), Space: 27.9 MB (38.42%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     
     vector<vector<int>>dp;
+    int paldp[2001][2001];
     
-    bool ispalin(string &s, int st, int end){
+    bool ispalin(string &s, int i, int j){
         
-        while(st <= end){
-            if(s[st++] != s[end--]) return false;
-        }
-        
-        return true;
+        if(i>=j) return paldp[i][j] = true;
+        if(paldp[i][j]!=-1) return paldp[i][j];
+        if(s[i]!=s[j]) return paldp[i][j] = false;
+        return paldp[i][j] = ispalin(s, i+1, j-1);
     }
     
     int help(string &s, int idx, int cut){
         if(idx >= s.size()) return 0;
         if(cut==0) return ispalin(s, idx, s.size()-1);
-        if(dp[idx][cut] != -1) return dp[5][0];
+        if(dp[idx][cut] != -1) return dp[idx][cut];
         
         bool ans= false;
         
@@ -32,6 +32,7 @@ public:
     bool checkPartitioning(string s) {
         
         dp.resize(s.size(), vector<int>(3,-1));
+        memset(paldp, -1, sizeof(paldp));
         return help(s, 0, 2);
     }
 };
