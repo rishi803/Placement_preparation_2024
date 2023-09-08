15a442274837ce816c5bbe12e1d7c2da2842f295
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-09-08 16:27:33 +0530

    Time: 2805 ms (5.09%), Space: 84.1 MB (28.91%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
     
-    int dp[71][5000];
+    int ans= INT_MAX;
+    // int res= INT_MAX;
+    int temp= INT_MAX;
+    vector<vector<int>>dp;
     
-    int help(vector<vector<int>>&mat,int row,int col,int currow,int target,int sum){
+    int help(vector<vector<int>>&mat, int target, int x, int y, int sum){
         
-        if(currow>=col) {
-           return abs(sum-target);
-        }
-        if(dp[currow][sum]!=-1) return dp[currow][sum];
-       
-        int mn=INT_MAX;
-        
-        for(int i=0;i<mat[currow].size();i++){
-          
-            mn=min(mn,help(mat,row,col,currow+1,target,sum+mat[currow][i]));
+        if(x >= mat.size()){
+         
+            return abs(target-sum);
         }
         
-        return dp[currow][sum]=mn;
+        if(dp[x][sum] != -1) return dp[x][sum];
+        
+        
+        int res= INT_MAX;
+        
+        for(int i= 0; i<mat[x].size(); i++){
+            res= min(res, help(mat, target, x+1, i,sum+mat[x][i]));
+        }
+        // cout<<endl;
+        
+        return dp[x][sum]= res;
     }
     
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
         
-       memset(dp,-1,sizeof(dp));
-        int row_h=mat[0].size(),col_v=mat.size();
-       return help(mat,row_h,col_v,0,target,0);
-     
+        dp.resize(mat.size(), vector<int>(5000,-1));
+          
+        for(int j=0; j<mat[0].size(); j++){
+              // cout<<mat[0][j]<<" ";
+            ans= min(ans,help(mat, target, 0, j, 0));
+          
+        }
+        
+        return ans;
     }
 };
