2cf36d5d1fb93604c3be1dfc7d13a7a655ac5203
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-09-09 23:34:32 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (85.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     
-    vector<vector<int>>dp;
+    vector<int>dp;
     
-    int help(vector<int>&nums, int target, int idx){
-        
-        if(idx >= nums.size()) return 0;
+    int help(vector<int>&nums, int target){
+    
         if(target < 0) return 0;
         if(target == 0) return 1;
-        if(dp[idx][target] != -1) return dp[idx][target];
+        if(dp[target] != -1) return dp[target];
         int ways= 0;
         
         for(int i=0; i<nums.size(); i++){
-            ways += help(nums, target-nums[i], i);
+            ways += help(nums, target-nums[i]);
         }
         
-        return dp[idx][target]= ways;
+        return dp[target]= ways;
     }
     
     int combinationSum4(vector<int>& nums, int target) {
         
-        dp.resize(nums.size()+1, vector<int>(target+1, -1));
-        return help(nums, target, 0);
+        dp.resize(target+1, -1);
+        return help(nums, target);
     }
 };
