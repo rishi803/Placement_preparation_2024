d47cc839b03d06abba4db16410a661db728ea509
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-09-10 21:36:38 +0530

    Time: 125 ms (83.56%), Space: 62 MB (81.04%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,74 +1,40 @@
-// class Solution {
-// public:
-    
-//     void dfs(int x,int y,int row,int col,vector<vector<int>>&land,set<pair<int,int>>&temp){
-//         if(x<0 or x>=row or y<0 or y>=col or land[x][y]==0) return;
-        
-//         land[x][y]=0;
-//         temp.insert({x,y});
-        
-//         dfs(x+1,y,row,col,land,temp);
-//         dfs(x,y+1,row,col,land,temp);
-//         dfs(x-1,y,row,col,land,temp);
-//         dfs(x,y-1,row,col,land,temp);
-        
-//     }
-    
-//     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-//         int row=land.size();
-//         int col=land[0].size();
-//         vector<vector<int>>ans;
-        
-//         for(int i=0;i<row;i++){
-//             for(int j=0;j<col;j++){
-//                 if(land[i][j]==1){
-//                     set<pair<int,int>>temp;
-//                     dfs(i,j,row,col,land,temp);
-//                     int tlx=temp.begin()->first,tly=temp.begin()->second;
-//                     auto it=--temp.end();
-//                     int brx=it->first,bry=it->second;
-//                     ans.push_back({tlx,tly,brx,bry});
-//                 }
-//             }
-//         }
-        
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
-    void dfs(int x, int y, int row, int col, vector<vector<int>>& land, vector<int>& temp) {
-        if (x < 0 || x >= row || y < 0 || y >= col || land[x][y] == 0)
-            return;
-
-        land[x][y] = 0;
-        temp[0] = min(temp[0], x);
-        temp[1] = min(temp[1], y);
-        temp[2] = max(temp[2], x);
-        temp[3] = max(temp[3], y);
-
-        dfs(x + 1, y, row, col, land, temp);
-        dfs(x, y + 1, row, col, land, temp);
-        dfs(x - 1, y, row, col, land, temp);
-        dfs(x, y - 1, row, col, land, temp);
+    
+    
+    void help(vector<vector<int>>&land, int i, int j, int &row, int &col){
+        if(i<0 or j<0 or i>=land.size() or j>=land[0].size() or land[i][j]==0) return;
+        
+        land[i][j]= 0;
+        row= max(row, i);
+        col= max(col, j);
+        
+        help(land, i+1, j, row, col);
+        help(land, i, j+1, row, col);
+        help(land, i-1, j, row, col);
+        help(land, i, j-1, row, col);
+        
     }
-
+    
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        int row = land.size();
-        int col = land[0].size();
-        vector<vector<int>> ans;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (land[i][j] == 1) {
-                    vector<int> temp = { i, j, i, j };  // {tlx, tly, brx, bry}
-                    dfs(i, j, row, col, land, temp);
-                    ans.push_back(temp);
-                }
-            }
-        }
-
+        
+        vector<vector<int>>ans;
+        
+          for(int i=0; i<land.size(); i++){
+              for(int j=0; j<land[0].size(); j++){
+                  if(land[i][j]==1){
+                      int r1= i;
+                      int c1= j;
+                      int r2= 0;
+                      int c2= 0;
+                      
+                      help(land, i, j, r2, c2);
+                      
+                      ans.push_back({r1,c1,r2,c2});
+                  }
+              }
+          }
+        
         return ans;
     }
 };
