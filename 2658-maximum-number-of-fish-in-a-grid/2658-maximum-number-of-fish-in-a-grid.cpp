32a27dbe4d44b8415de325acf603829df96e961b
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-09-10 23:03:00 +0530

    Time: 51 ms (85.70%), Space: 88.7 MB (75.86%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     
+    vector<int>dir= {-1,0,1,0,-1};
     int ans= 0;
     
     void help(vector<vector<int>>&grid, int i, int j, int &fish){
            if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or grid[i][j] == 0) return;
         
-      
+           
         
               fish+= grid[i][j];
               grid[i][j]= 0;
         
-         help(grid, i+1, j, fish);
-         help(grid, i, j+1, fish);
-         help(grid, i-1, j, fish);
-         help(grid, i, j-1, fish);
+        for(auto it=0; it<4; it++){
+            int x= i+ dir[it];
+            int y= j+ dir[it+1];
+            
+            
+            help(grid, x, y, fish);
+        }
         
         
     }
