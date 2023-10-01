9f1b035c734554a989e67ba96e1fb95e3405587a
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-10-02 00:17:51 +0530

    Time: 6 ms (34.26%), Space: 14.1 MB (30.26%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -2,32 +2,21 @@
 class Solution {
 public:
     
-    int ans= 0;
-    
+    int cnt= 0;
     int help(TreeNode* root){
         if(!root) return 0;
         
-//         root->val+= help(root->left);
-//         root->val+=help(root->right);
-        
-//         int remain= root->val - 1;
-        
-//         ans+= abs(remain);
-        
-//         return remain;
-        
         int left= help(root->left);
-        int right= help(root->right);
+        int right=help(root->right);
         
-        ans+= abs(left) + abs(right);
+        cnt+= abs(left) + abs(right);
         
-        return root->val + left + right - 1;
-        
+        return root->val + (left + right) - 1;
     }
     
     int distributeCoins(TreeNode* root) {
         
          help(root);
-        return ans;
+        return cnt;
     }
 };
