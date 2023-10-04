bff980dc3e5a3d8c8184fd16c2ae3168f64204bd
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-10-04 18:21:50 +0530

    Time: 91 ms (48.56%), Space: 61.5 MB (70.46%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,18 +1,38 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+        
+//         for(int i=0; i<nums.size(); i++){
+            
+//             while(nums[i] != i and nums[i] != nums[nums[i]] ){
+//                 swap(nums[i], nums[nums[i]]);
+//             }
+//         }
+        
+//         for(int i=0; i<nums.size(); i++){
+//             if(nums[i] != i) return nums[i];
+//         }
+        
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         
         for(int i=0; i<nums.size(); i++){
             
-            while(nums[i] != nums[nums[i]] ){
-                swap(nums[i], nums[nums[i]]);
+            while(nums[i] != i+1 and nums[i] != nums[nums[i] - 1] ){
+                swap(nums[i], nums[nums[i] -1]);
             }
         }
         
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] != i) return nums[i];
+            if(nums[i] != i+1) return nums[i];
         }
         
         return -1;
     }
 };
+
