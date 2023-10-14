2696e7dd4ee2f9c9dde457970fc88f57f41f644f
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-10-14 21:39:23 +0530

    Time: 261 ms (30.00%), Space: 122 MB (10.00%) - LeetHub

---
+++ 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+  vector<string> getWordsInLongestSubsequence(int n, vector<string>& words, vector<int>& groups) {
+        vector<vector<string>> dp(n); // dp[i] stores the longest subsequence ending at index i
+
+        for (int i = 0; i < n; i++) {
+            dp[i].push_back(words[i]);
+        }
+
+        int maxLength = 1; // Initialize with the minimum length
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (groups[j] != groups[i] && words[j].size() == words[i].size()) {  // checking the condition given in problem
+                    int hamDist = 0;
+                    for (int k = 0; k < words[i].size(); k++) {
+                        if (words[j][k] != words[i][k]) {
+                            hamDist++;
+                            if (hamDist > 1) break;
+                        }
+                    }
+
+                    if (hamDist == 1) {           // if all condition satisfied
+                        if (dp[j].size() + 1 > dp[i].size()) {    // and check whether from this index you are getting max length subsequence
+                            dp[i] = dp[j]; // Copy the longest subsequence found so far  
+                            dp[i].push_back(words[i]);
+                            maxLength = max(maxLength, int(dp[i].size()));
+                        }
+                    }
+                }
+            }
+        }
+
+        vector<string> longestSubsequence;
+        for (int i = 0; i < n; i++) {
+            if (dp[i].size() == maxLength) {             
+                longestSubsequence = dp[i];
+                break; // Break when the first longest subsequence is found
+            }
+        }
+
+        return longestSubsequence;
+    }
+};
