f2cd1c98d876f09188520e2fc9e838ffeb57e125
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-10-21 20:14:16 +0530

    Time: 108 ms (48.38%), Space: 90.3 MB (12.76%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -1,21 +1,29 @@
-// very much similar to https://leetcode.com/problems/find-all-good-indices/
-
 class Solution {
 public:
-    vector<int> goodDaysToRobBank(vector<int>&nums, int time) {
+    vector<int> goodDaysToRobBank(vector<int>& nums, int time) {
         
-        int sz=nums.size();
-        vector<int>pref(sz,0),suff(sz,0),ans;
-      
-        for(int i=sz-2;i>=0;i--){
-            if(nums[i+1]<=nums[i]) pref[i]=pref[i+1]+1;
+        int sz= nums.size();
+        
+        vector<int>prefix(sz,1),suffix(sz,1);
+        
+        for(int i=1; i<sz; i++){
+            if(nums[i] <= nums[i-1]) prefix[i]= 1+prefix[i-1];
         }
-        for(int i=1;i<sz;i++){
-            if(nums[i]>=nums[i-1]) suff[i]=suff[i-1]+1;
+        
+        for(int i=sz-2; i>=0; i--){
+            if(nums[i] <= nums[i+1]) suffix[i]= 1+suffix[i+1];
         }
-        for(int i=time;i<sz-time;i++){
-          if(pref[i-time]>=time and suff[i+time]>=time) ans.push_back(i);
+        
+        vector<int>ans;
+        
+        for(int i=0; i<sz; i++){
+            
+            int decreasing= prefix[i]-1;
+            int increasing= suffix[i]-1;
+            
+            if(decreasing >= time and increasing >= time) ans.push_back(i);
         }
+        
         return ans;
     }
 };
