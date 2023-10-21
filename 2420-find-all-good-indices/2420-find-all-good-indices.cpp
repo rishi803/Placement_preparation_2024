26018ec4ed2f39053bd6df7b6af0746a54615788
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-10-21 17:45:19 +0530

    Time: 105 ms (60.68%), Space: 89.4 MB (13.35%) - LeetHub

--- 2420-find-all-good-indices/2420-find-all-good-indices.cpp
+++ 2420-find-all-good-indices/2420-find-all-good-indices.cpp
@@ -1,19 +1,18 @@
-// very much similar to https://leetcode.com/problems/find-good-days-to-rob-the-bank/
-
 class Solution {
 public:
     vector<int> goodIndices(vector<int>& nums, int time) {
-         int sz=nums.size();
+        
+        int sz=nums.size();
         vector<int>pref(sz,1),suff(sz,1),ans;
       
         for(int i=sz-2;i>=0;i--){
-            if(nums[i]<=nums[i+1]) suff[i]=suff[i+1]+1;
+            if(nums[i+1]<=nums[i]) pref[i]=pref[i+1]+1;
         }
         for(int i=1;i<sz;i++){
-            if(nums[i]<=nums[i-1]) pref[i]=pref[i-1]+1;
+            if(nums[i]>=nums[i-1]) suff[i]=suff[i-1]+1;
         }
         for(int i=time;i<sz-time;i++){
-          if(pref[i-1]>=time and suff[i+1]>=time) ans.push_back(i);
+          if(pref[i-time]>=time and suff[i+time]>=time) ans.push_back(i);
         }
         return ans; 
     }
