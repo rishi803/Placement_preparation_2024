6ebe9c58f032b9ad2f876d10d59b11b4f8771802
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-10-22 23:01:29 +0530

    Time: 120 ms (91.67%), Space: 89.7 MB (80.92%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -2,35 +2,33 @@ class Solution {
 public:
     int maximumScore(vector<int>& nums, int k) {
         int left= k, right=k, ans= nums[k], mn= nums[k], sz= nums.size();
-        int leftidx=k, rightidx=k;
         
-        while((left > 0) or (right < sz-1))
+        while((left > 0) or (right < sz-1))  // jha khde ho uske left aur right dekho
         {
-           if(left == 0 and right < sz-1){
+           if(left == 0 and right < sz-1){  // agr left edge pr ho to na chahte hue right vala lena pdega
                 right++;
                mn= min(mn, nums[right]);
               
            }
             
-            else if(right == sz-1 and left > 0){
-                 left--;
+            else if(right == sz-1 and left > 0){ // agr right edge pr ho to na chahte hue left vala lena pdega
+                 left--; 
                 mn= min(mn, nums[left]);
                
             }
             
-            else if ( nums[left-1] > nums[right+1]){
+            else if ( nums[left-1] > nums[right+1]){  // agr left ke value bdi to idhar hi jao mn ko maximise krne ke liye
                 left--;
                 mn= min(mn, nums[left]);
             }
             
-            else{
+            else{            // right ki trf jao mn ko maximise krne ke liye
                 right++;
                 mn= min(mn, nums[right]);
             }
             // cout<<left<<" "<<right<<" "<<mn<<endl;
             ans= max(ans, mn * (right-left+1));
             
-            // cout<<leftvalue<<" "<<rightvalue<<" "<<leftidx<<" "<<rightidx<<" "<<mn<<endl;
         }
         
         return ans;
