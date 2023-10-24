ac6b68152ca5c7efc236946292b7196c04e980ac
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-10-25 02:09:40 +0530

    Time: 6 ms (84.80%), Space: 22.5 MB (33.12%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -1,10 +1,32 @@
 
 class Solution {
 public:
+    
+     vector<int>ans;
+    
+    void help(TreeNode* root, int level){
+        if(!root) return;
+        
+      
+    // If the vector size is not enough, resize it
+    if (ans.size() <= level) {
+        ans.push_back(INT_MIN);
+    }
+
+    // Update the maximum value for the current level
+    ans[level] = max(ans[level], root->val);
+
+    // Recursively process the left and right subtrees
+     help(root->left, level + 1);
+     help(root->right, level + 1);
+    }
+    
     vector<int> largestValues(TreeNode* root) {
         
         if(!root) return {};
-        vector<int>ans;
+        help(root,0);
+        return ans;
+       
         queue<TreeNode*>q;
         q.push(root);
    
