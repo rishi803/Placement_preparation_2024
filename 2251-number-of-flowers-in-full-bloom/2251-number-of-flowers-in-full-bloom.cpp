915de517f626b8d6fcbf3b2b03cc3c6c870cbb9b
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-10-26 18:48:52 +0530

    Time: 270 ms (28.73%), Space: 87.5 MB (44.54%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -7,39 +7,36 @@ public:
         for(int i=0; i<flowers.size(); i++){
             mp[flowers[i][0]]++;
             mp[flowers[i][1]+1]--;
-            // mxsz= max({mxsz,flowers[i][0],flowers[i][1]});
         }
      
-        set<pair<int,int>>v;
         int flowercnt= 0;
         
-       for(auto [idx,flowers]: mp){
-           flowercnt+= flowers;
-           v.insert({idx,flowercnt});
+       for(auto [key,value]: mp){
+           flowercnt+= value;
+           mp[key]= flowercnt;
        }
-        
-        // for(auto i:v){
-        //     cout<<i.first<<" "<<i.second<<endl;
-        // }
-        vector<int>result;
-        
-        for(int i=0; i<people.size(); i++){
-          std::set<std::pair<int, int>>::iterator ans = v.lower_bound({people[i],0});
+        vector<int> result;
+
+        for (int i = 0; i < people.size(); i++) {
+            auto ans = mp.lower_bound(people[i]);
+
+            if (ans == mp.begin() and ans->first != people[i])
+              {
+                 result.push_back(0);
+             }
             
-            if(ans == v.begin() and ans->first != people[i]){
-                result.push_back(0);
-            }
-            
-            else if(ans->first != people[i] and ans != v.begin()) {
+            else if( ans != mp.begin() and ans->first != people[i])
+            {
                 ans--;
                 result.push_back(ans->second);
-                
             }
+            
             else{
                 result.push_back(ans->second);
             }
+            
         }
-        
+
         return result;
     }
 };
