d3125b84fe6d99f32f441a7bf0e70f5f39b44b12
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-10-27 23:11:11 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (72.80%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,51 +1,38 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-       vector<vector<bool>>dp(s.size(),vector<bool>(s.size(),0));
-        int mx=0,x=0,y=0;
-        string ans;
-        int k=0;
-        for(int cnt=0;cnt<s.size();cnt++){
-            int i=0;
-            int j=cnt;
-            for(int times=0;times<(s.size()-cnt);times++){
-                // cout<<times<<" ";
-                // cout<<" i= "<<i<<" j= "<<j<<" ";
-                int gap=j-i;
-                
-                if(gap==0){
-                    dp[i][j]=true;
-                }
-                
-                else if(gap==1){
-                    if(s[i]==s[j]){
-                        dp[i][j]=true;
-                         if(gap>mx){
-                            mx=gap;
-                            x=i;
-                            y=j;
-                        }
-                    }
-                   
-                }
-                else{
-                    if(dp[i+1][j-1]==true and s[i]==s[j]){
-                        dp[i][j]=true;
-                        if(gap>mx){
-                            mx=gap;
-                            x=i;
-                            y=j;
-                        }
-                    }
-                   
-                }
-                i++;
-                j++;
-            }
-            // cout<<endl;
+       int n = s.length();
+        if (n <= 1) {
+            return s;
         }
-        for(int i=x;i<=y;i++) ans+=s[i];
-        
-       return ans;
+
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < n;) {
+            int left = i;
+            int right = i;
+
+            // Skip identical characters
+            while (right < n - 1 && s[right] == s[right + 1]) {
+                right++;
+            }
+
+            i = right + 1;
+
+            // Expand around the center for odd-length palindromes
+            while (left > 0 && right < n - 1 && s[left - 1] == s[right + 1]) {
+                left--;
+                right++;
+            }
+
+            // Check if the current palindrome is longer than the previous maxLen
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+        }
+
+        return s.substr(start, maxLen);
     }
 };
