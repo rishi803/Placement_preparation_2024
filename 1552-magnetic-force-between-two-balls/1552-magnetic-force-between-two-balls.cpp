477eac22d2bbf440cace1e8de6ae838601eb9bd6
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-11-01 23:09:37 +0530

    Time: 178 ms (19.47%), Space: 58.1 MB (55.31%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    bool isvalid(vector<int>&pos, int balls, int midforce){
+        int cnt = 1;
+        int force= pos[0]+midforce;
+        
+        for(int i=1; i<pos.size(); i++){
+            
+            if(pos[i] >= force){
+                cnt++;
+                force= pos[i] + midforce;
+            }
+        }
+        
+        return cnt >= balls;
+    }
+    
+    int maxDistance(vector<int>& position, int m) {
+        
+       sort(position.begin(),  position.end());
+       
+        int low= 1;
+        int high= 1e9;
+        int ans= 0;
+        
+        while(low <= high){
+            int mid= low + (high-low)/2;
+            
+            if(isvalid(position,m,mid)){
+                ans= mid;
+                low= mid+1;
+            }
+            else{
+                high= mid-1;
+            }
+        }
+        
+        return ans;
+    }
+};
