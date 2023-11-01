c479866538270d287ca0cd63915a6248d41c3675
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-11-02 03:23:13 +0530

    Time: 15 ms (29.18%), Space: 21 MB (13.19%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -6,15 +6,15 @@ public:
         vector<int>prefix(sz);
         vector<int>suffix(sz);
         
-        prefix[0]= 0;
-        suffix[sz-1]= 0;
+        prefix[0]= height[0];
+        suffix[sz-1]= height[sz-1];
         
         for(int i=1; i<sz; i++){
-            prefix[i]= max(prefix[i-1], height[i-1]);
+            prefix[i]= max(prefix[i-1], height[i]);
         }
         
         for(int i= sz-2; i>=0; i--){
-            suffix[i]= max(suffix[i+1], height[i+1]);
+            suffix[i]= max(suffix[i+1], height[i]);
         }
         
         int area= 0;
@@ -23,7 +23,7 @@ public:
             
             if(height[i] < prefix[i] and height[i] < suffix[i]){
             
-                int pani= min(prefix[i], suffix[i]);
+                int pani= min(prefix[i-1], suffix[i+1]);
                 area= area + (pani-height[i]);
             }
             // cout<<height[i]<<" "<<prefix[i]<<" "<<suffix[i]<<endl;
