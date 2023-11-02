916c010df5c745dfb599dacaae4c7dfe64c06b8e
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-11-03 03:01:15 +0530

    Time: 315 ms (11.98%), Space: 95.5 MB (8.94%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     
-    
-    int mod=int(1e9+7);
-    
-    int help(int idx,int mostpeo,int minpro,vector<int>&group,vector<int>&profit,vector<vector<vector<long long>>>&dp){
-        if(mostpeo<0) return 0;
-        if(idx==profit.size()){
-            if(minpro>0) return 0;
-            else return 1;
-        }
-        if(minpro<0) minpro=0;
-        if(dp[idx][minpro][mostpeo]!=-1) return dp[idx][minpro][mostpeo];
+    int help(int n, int mnpr, vector<int>&grp, vector<int>&pr, int idx, long long gained, int people, vector<vector<vector<long long>>>&dp){
         
-        long long chorikrle= help(idx+1,mostpeo-group[idx],minpro-profit[idx],group,profit,dp);
-        long long matkr= help(idx+1,mostpeo,minpro,group,profit,dp);
-            
-            
-        return dp[idx][minpro][mostpeo]=(chorikrle+matkr)%(mod);
-       
+         if(gained >= mnpr) gained= mnpr;
+        if(idx == grp.size() and gained >= mnpr and people <= n) return 1;
+        if(people > n or idx >= grp.size()) return 0;
+         if(dp[idx][gained][people]!=-1) return dp[idx][gained][people];
         
+      
+        
+        int chori_kar= help(n, mnpr, grp, pr, idx+1, gained+pr[idx], people+grp[idx], dp) % int(1e9+7);
+        int chori_mt_kar= help(n, mnpr, grp, pr, idx+1, gained, people, dp) % int(1e9+7);
+            
+        return dp[idx][gained][people]= (chori_kar + chori_mt_kar) % int(1e9+7);
+         
     }
+    
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
         
-        vector<vector<vector<long long>>>dp(group.size(),vector<vector<long long>>(minProfit+1,vector<long long>(n+1,-1)));
-        return help(0,n,minProfit,group,profit,dp);
+            vector<vector<vector<long long>>>dp(group.size()+1,vector<vector<long long>>(minProfit+2,vector<long long>(n+1,-1)));
+        return help(n, minProfit, group, profit, 0, 0,0,dp);
     }
 };
