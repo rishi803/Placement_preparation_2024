40bbdd47b30a1e80222a05967e69e85399098017
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-11-07 13:24:29 +0530

    Time: 22 ms (92.43%), Space: 7.9 MB (78.84%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -2,29 +2,20 @@ class Solution {
 public:
     
     int dp[501][501];
-    int help(string &s,int st,int end){
+    
+    int help(string &s,int st, int end){
+        
+        if(st > end) return 0;
+        if(s[st] == s[end]) return help(s, st+1, end-1);
+        if(dp[st][end] != -1) return dp[st][end];
+        
+        return dp[st][end]= min(1+help(s, st+1, end), 1+help(s, st, end-1));
        
-        if(st>=end) return 0;
-        if(dp[st][end]!=-1) return dp[st][end];
-        
-        int noinsert=INT_MAX,insert=INT_MAX;
-        
-            if(s[st]==s[end]){
-                
-                noinsert=help(s,st+1,end-1);
-            }
-            
-            else{
-                insert=1+min(help(s,st+1,end),help(s,st,end-1));
-            }
-        
-        return dp[st][end]=min(insert,noinsert);
-        
     }
     
     int minInsertions(string s) {
         
         memset(dp,-1,sizeof(dp));
-        return help(s,0,s.size()-1);
+        return  help(s,0,s.size()-1);
     }
 };
