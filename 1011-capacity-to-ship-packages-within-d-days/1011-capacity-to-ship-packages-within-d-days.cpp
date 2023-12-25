29b2a3ff34630f912fb860ef7dd51d4c3a2115ee
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-12-25 19:22:31 +0530

    Time: 49 ms (76.06%), Space: 31.5 MB (42.74%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
-    bool isValid(int mid_weight,vector<int>&weights,int days){
-        int cnt=1,sum=0;
+    
+    bool isvalid(int mid_weight, vector<int>& weights ,int days){
         
-        for(int i=0;i<weights.size();i++){
-            sum+=weights[i];
+        int cnt= 1;
+        int sum= 0;
+        
+        for(int i=0; i<weights.size(); i++){
             
-            if(sum>mid_weight){
+            sum= sum + weights[i];   
+            
+            if(sum > mid_weight) {
                 cnt++;
-                sum=weights[i];
+                sum= weights[i];     // sum= 6
             }
         }
-        return cnt<=days;
+        
+        return cnt <= days;
+        
     }
     
     int shipWithinDays(vector<int>& weights, int days) {
         
-        int low=*max_element(weights.begin(),weights.end());
-        int high= accumulate(weights.begin(),weights.end(),0);
-        
-        int ans=0;
+        int low= *max_element(weights.begin(), weights.end());        // best case
+        int high= accumulate(weights.begin(), weights.end(), 0);   // worst case
+        int ans= 0;
         
         while(low<=high){
             
-            int mid_weight= low+(high-low)/2;
+            int mid_weight= low + (high-low)/2;         // w1, w2 w3 w4  w5 w6
             
-            if(isValid(mid_weight,weights,days)){
-                ans=mid_weight;
-                high=mid_weight-1;
+            if(isvalid(mid_weight, weights, days)){
+                 ans= mid_weight;
+                 high= mid_weight-1;     // maximize the minimum
             }
             
             else{
-                low=mid_weight+1;
+                low= mid_weight+1;
             }
+            
         }
         
         return ans;
