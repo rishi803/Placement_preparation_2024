59e919d6866d73a833ceff5e8bdeb952b99f9341
rishi singh <85188638+rishi803@users.noreply.github.com>
2024-01-13 23:51:17 +0530

    Time: 169 ms (39.16%), Space: 84.6 MB (95.18%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     
-    bool isValid(vector<int>&candy,long long k,int mid){
-        
-        long long cnt=0;
-        
-        for(int i=0;i<candy.size();i++){
-            
-            int distribute=(candy[i]/mid);
-            cnt+=distribute;
+    
+    bool isvalid(vector<int>&candies, int mid, long long k){
+        long long cnt= 0;
+
+        for(int i=0; i<candies.size(); i++){
+
+            cnt+= candies[i]/mid;
         }
-        if(cnt>=k) return true;
-        else return false;
+        
+        return cnt>=k;
+        
     }
     
     int maximumCandies(vector<int>& candies, long long k) {
         
-        long long total=accumulate(candies.begin(),candies.end(),0ll);
-        if((total/k)<=0) return 0;
+        long long total= accumulate(candies.begin(), candies.end(),0ll);
+        if((total/k) <= 0) return 0;
         
         int low=1;
-        int high=*max_element(candies.begin(),candies.end());
-        int ans=0;
+        int high= *max_element(candies.begin(), candies.end());
+        int ans= 0;
         
-        while(low<=high){
+        while(low <= high){
+            int mid= low + (high-low) / 2;
             
-            int mid=low+(high-low)/2;
-            
-            if(isValid(candies,k,mid)){
-                ans=mid;
-                low=mid+1;
+           if (isvalid(candies, mid, k)){
+                ans= mid;
+                low= mid+1;
             }
-            else{                                      
-
-                high=mid-1;
+            
+            else{
+                high= mid-1;
             }
         }
         
