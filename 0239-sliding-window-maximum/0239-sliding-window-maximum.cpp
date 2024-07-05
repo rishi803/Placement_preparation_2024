bf0e67efb7a641146f6bf49e9bcc509260a3bfbb
rishi singh <85188638+rishi803@users.noreply.github.com>
2024-07-05 15:45:16 +0530

    Time: 162 ms (82.10%), Space: 137.1 MB (69.87%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        map<int,int,greater<int>>mp;
-        vector<int>ans;
-        
-        for(int i=0; i<k; i++){
-            mp[nums[i]]++;
-        }
-        
-        ans.push_back(mp.begin()->first);
-        
-        for(int i=k; i<nums.size(); i++){
-            mp[nums[i-k]]--;
-            if(mp[nums[i-k]]==0) mp.erase(nums[i-k]);
-            
-            mp[nums[i]]++;
-            
-            ans.push_back(mp.begin()->first);
-        }
-        return ans;
-    }
+       deque<int> queue;
+       vector<int> ans;
+       for (int left = 0; left < nums.size(); ++left) {
+           // As the window move on, element nums[left-k] will be outdated.
+           if (queue.front() == left - k) queue.pop_front();
+           // Now we are ready to push our new element nums[left]'s index into the queue.
+           // But before that, we should clear elements which is smaller then nums[left].
+           // Why? Because if nums[left] is bigger then nums[i], 
+           // there will be no way for nums[i] be selected as the max number in range (left-k, left]
+           while (!queue.empty() && nums[queue.back()] < nums[left]) queue.pop_back();
+           // Now push the index into our queue.
+           queue.push_back(left);
+           // Okay, now nums[queue.front()] mush be the max number in range (left-k, left] 
+           if (left - k + 1 >= 0) ans.push_back(nums[queue.front()]);
+       }
+       return ans;
+   }
 };
