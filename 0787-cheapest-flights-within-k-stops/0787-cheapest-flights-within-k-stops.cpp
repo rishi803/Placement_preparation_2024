4b4b2e3716e3b687f94138c4f94bbbc40e4a005f
rishi singh <85188638+rishi803@users.noreply.github.com>
2024-07-21 01:19:08 +0530

    Time: 9 ms (95.95%), Space: 16.9 MB (64.76%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,87 +1,37 @@
-// Input: n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1
-
-
-
-// 0----> {1,100}                 
-// 1----> {2,100} {3,600}
-// 2----> {0,100} {3,200}
-
-// idx =    0          1          2          3             (initially)
-// cost=    0        INT_MAX    INT_MAX    INT_MAX
-
-
-
-/*
-queue =  [  [{0,0}] ] 
-stops =        0     
-    
-idx =    0          1          2          3           (for node 0 update node 1 and also update minCost) 
-cost=    0         100   INT_MAX    INT_MAX
-    
-*/
-/*
-
-queue =  [ [{1,100}] ]                              (for node 1 update node 2 and node 3 and also update minCost)
-stops =        1
-    
-idx =    0          1          2          3            
-cost=    0          100       200        700
-    
-*/
-/*
-    
-queue =  [  [{2,200}  {3,700}]  ]     (return mn_cost because we have used total stops)
-stops =         2        2
-
-*/
-
 
 
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> graph(n);       // vector<vector<dest,cost>>
-        for(auto e : flights) {
-            graph[e[0]].push_back({e[1], e[2]});
-        }
-        int ans=INT_MAX;
-        vector<int> cost(n, INT_MAX);
-        cost[src]=0;
-        queue<pair<int, int>> q;                       
-        q.push({src, 0});                      // q<pair<dest,initial_cost>>
-        
-        int stops= 0;
-        
-        while(!q.empty() and stops <= k){
-            
-            int sz= q.size();
-            
-            while(sz--){
-                
-                int cur_node= q.front().first;
-                int abhi_tak_cost= q.front().second;
-                q.pop();
-                
-                for(auto &child : graph[cur_node]){
-                    if(cost[child.first] < abhi_tak_cost + child.second) continue;  // agar is node par phle se koi choti cost ka rasta tha to aj isse jyada cost se kun jaye vapas lauto
-                    
-                    if(child.first == dst) ans= min(ans, abhi_tak_cost + child.second); // answer utha lo agar destination yhi h isse age vaise bhi nhi jana 
-                    
-                    else{
-                        cost[child.first]= abhi_tak_cost + child.second;
-                        q.push({child.first, abhi_tak_cost + child.second});
-                    }
-                    
-                    
-                }
-                
+    vector<vector<pair<int, int>>> route;
+    vector<vector<int>> memo;
+
+    int ans(int src, int dst, int k) {
+        if (k < 0) return INT_MAX; // No more stops allowed
+        if (src == dst) return 0;  // Reached destination
+        if (memo[src][k] != -1) return memo[src][k]; // Check if already computed
+
+        int res = INT_MAX;
+        for (auto &node : route[src]) {
+            int next_node = node.first;
+            int price = node.second;
+            int sub_problem = ans(next_node, dst, k - 1);
+            if (sub_problem != INT_MAX) {
+                res = min(res, price + sub_problem);
             }
-            
-            stops++;
-                
         }
-        
-        if(ans == INT_MAX) return -1;
-        return ans;
+
+        return memo[src][k] = res; // Memorize the result
+    }
+
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        route.resize(n);
+        for (auto &flight : flights) {
+            route[flight[0]].emplace_back(flight[1], flight[2]);
+        }
+
+        memo.assign(n, vector<int>(k + 2, -1)); // Initialize memoization table
+
+        int result = ans(src, dst, k + 1);
+        return result == INT_MAX ? -1 : result;
     }
 };
